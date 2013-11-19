4faff7d8541d79eb4ab96b1cf5ce9f1ce338b47c
Srinivasan Chandrasekharan <schandra@noao.edu>
2013-11-19 14:39:37 -0700

    Adding working version of Database and Table

--- python/lsst/sims/operations/maf/db/Database.py
+++ python/lsst/sims/operations/maf/db/Database.py
@@ -1,29 +1,52 @@
 __author__ = 'schandra'
 
-import lsst.sims.catalogs.generation.db import DBOject, ChunkIterator
-import numpy
-from sqlalchemy import func
-from sqlalchemy.sql import expression
+from lsst.sims.operations.maf.db.Table import Table
 
 class Database:
-
-    def __init__(self, connString, username, password, sessionID):
-        pass
+    def __init__(self, dbAddress, sessionID, tables=[['Config','configID'],
+						     ['Session', 'sessionID'],
+						     ['Proposal', 'propID'], 
+						     ['Config_File', 'config_fileID'],
+						     ['Log', 'logID'],
+                                                     ['TimeHistory', 'timeHistID'], 
+						     ['Cloud', 'cloudID'],
+						     ['Seeing', 'seeingID'],
+						     ['Proposal_Field', 'proposal_field_id'],
+                                                     ['Field', 'fieldID'], 
+						     ['SeqHistory', 'sequenceID'],
+						     ['MissedHistory', 'missedHistID'],
+						     ['ObsHistory', 'obsHistID'],
+						     ['SeqHistory_MissedHistory', 'seqhistory_missedHistID'],
+						     ['SeqHistory_ObsHistory', 'seqhistory_obsHistID'],
+                                                     ['ObsHistory_Proposal', 'obsHistory_propID'],
+						     ['SlewActivities', 'slewActivityID'],
+						     ['SlewHistory', 'slewID'],
+                                                     ['SlewMaxSpeeds', 'slewMaxSpeedID'],
+						     ['SlewState', 'slewIniStatID']]):
+        self.dbAddress = dbAddress
+        self.sessionID = sessionID
+        self.tableRegistry = {}
+        for table in tables:
+            print "Creating registry entry for [%s %s]" % (table[0], table[1])
+            self.tableRegistry[table[0]] = Table(table[0], table[1], dbAddress)
 
     # ObsHistory Table functions
     def getObservations(self):
-        pass
+	constraint = 'Session_sessionID = %d' % self.sessionID
+	return self.tableRegistry['ObsHistory'].query_columns(constraint = constraint)
 
     # ObsHistory_Proposal Table functions
     def getObservationsForPropID(self, propID):
-        pass
+	pass
 
     # SlewHistory Table functions
     def getSlews(self):
-        pass
+	constraint = 'ObsHistory_Session_sessionID = %d' % self.sessionID
+	return self.tableRegistry['SlewHistory'].query_columns(constraint = constraint)
 
     def getSlewForObsHistID(self, obsHistID):
-        pass
+	constraint = 'ObsHistory_Session_sessionID = %d and ObsHistory_obsHistID = %d' % (self.sessionID, obsHistID)
+	return self.tableRegistry['SlewHistory'].query_columns(constraint = constraint)
 
     # SlewState Table functions
     def getSlewStates(self):
@@ -48,15 +71,18 @@ class Database:
 
     # Proposal Table functions
     def getProposals(self):
-        pass
+        constraint = 'Session_sessionID = %d' % self.sessionID
+	return self.tableRegistry['Proposal'].query_columns(constraint = constraint)
 
     # Session Table functions
     def getSession(self):
-        pass
+	constraint = 'sessionID = %d' % self.sessionID
+        return self.tableRegistry['Session'].query_columns(constraint = constraint)
 
     # Config Table functions
     def getConfig(self):
-        pass
+        constraint = 'Session_sessionID = %d' % self.sessionID
+	return self.tableRegistry['Config'].query_columns(constraint = constraint)
 
     # Config_File Table functions
     def getConfigFile(self):
@@ -68,7 +94,8 @@ class Database:
 
     # TimeHistory Table functions
     def getTimeHistory(self):
-        pass
+	constraint = 'Session_sessionID = %d' % self.sessionID
+	return self.tableRegistry['TimeHistory'].query_columns(constraint = constraint)
 
     # Proposal_Field Table functions
     def getFieldsForPropID(self, propID):
@@ -76,15 +103,17 @@ class Database:
 
     # Field Table functions
     def getAllFields(self):
-        pass
+	return self.tableRegistry['Field'].query_columns()
 
     # MissedHistory functions
     def getMissedObservations(self):
-        pass
+        constraint = 'Session_sessionID = %d' % self.sessionID
+	return self.tableRegistry['MissedHistory'].query_columns(constraint = constraint)
 
     # SeqHistory functions
     def getSequences(self):
-        pass
+        constraint = 'Session_sessionID = %d' % self.sessionID
+        return self.tableRegistry['SeqHistory'].query_columns(constraint = constraint)
 
     def getObservationsForSeqID(self, seqID):
         pass
@@ -94,16 +123,12 @@ class Database:
 
     # Cloud
     def getCloudData(self):
-        pass
+        return self.tableRegistry['Cloud'].query_columns()
 
     # Seeing
     def getSeeingData(self):
-        pass
+	return self.tableRegistry['Seeing'].query_columns()
 
-    # Execute arbitrary SQL
-    def executeSQL(self, sql):
-        pass
 
-    # Disconnect from DB
-    def disconnect(self):
-        pass
+    def execute(self, tableName, colnames=None, chunk_size=None, constraint=None, numLimit=None):
+	return self.tableRegistry[tableName].query_columns(colnames, chunk_size, constraint, numLimit)
--- python/lsst/sims/operations/maf/db/Table.py
+++ python/lsst/sims/operations/maf/db/Table.py
@@ -1,1 +1,88 @@
-__author__ = 'schandra'
+__author__ = 'simon'
+
+from lsst.sims.catalogs.generation.db import DBObject, ChunkIterator
+import numpy
+from sqlalchemy import func
+from sqlalchemy.sql import expression
+
+class Table(DBObject):
+    skipRegistration = True
+
+    def __init__(self, tableName, idColKey, dbAddress):
+        """
+        Initialize an object for querying OpSim databases
+
+        Keyword arguments:
+        @param runtable: Name of the table to query
+        @param dbAddress: A string indicating the location of the data to query.
+                          This should be a database connection string.
+        """
+
+        if dbAddress is None:
+            dbAddress = self.getDbAddress()
+
+	self.idColKey = idColKey
+        self.dbAddress = dbAddress
+        self.tableid = tableName
+        self._connect_to_engine()
+        self._get_table()
+
+        if self.generateDefaultColumnMap:
+            self._make_default_columns()
+
+        self._make_column_map()
+        self._make_type_map()
+
+    def _get_column_query(self, colnames=None):
+        if colnames is None:
+            colnames = [k for k in self.columnMap.keys()]
+        try:
+            vals = [self.columnMap[k] for k in colnames]
+        except KeyError:
+            for c in colnames:
+                if c in self.columnMap.keys():
+                    continue
+                else:
+                    print("%s not in columnMap"%(c))
+            raise ValueError('entries in colnames must be in self.columnMap')
+
+        # Get the first query
+        idColName = self.columnMap[self.idColKey]
+        if idColName in vals:
+            idLabel = self.idColKey
+        else:
+            idLabel = idColName
+
+        #SQL server requires an aggregate on all columns if a group by clause is used.
+        #Modify the columnMap to take care of this.  I'm choosing MIN, but it shouldn't
+        #matter since the entries are almost identical (except for proposalId???)
+        #Added double-quotes to handle column names that start with a number.
+        query = self.session.query(func.min(self.table.c[idColName]).label(idLabel))
+        for col, val in zip(colnames, vals):
+            if val is idColName:
+                continue
+            #Check if this is a default column.
+            if val == col:
+                #If so, use the column in the table to take care of DB specific column
+                #naming conventions (capitalization, spaces, etc.)
+                query = query.add_column(func.min(self.table.c[col]).label(col))
+            else:
+                #If not, assume that the user has specified the column correctly
+                query = query.add_column(func.min(expression.literal_column(val)).label(col))
+        return query
+
+    def query_columns(self, colnames=None, chunk_size=None, constraint=None, groupByCol=None, numLimit=None):
+
+        query = self._get_column_query(colnames)
+        if constraint is not None:
+            query = query.filter(constraint)
+
+        if groupByCol is not None:
+            #Either group by a column that gives unique visits
+            query = query.group_by(groupByCol)
+        #else:
+            #or group by the unique id for the table.
+        #    query = query.group_by(self.idColKey)
+        if numLimit:
+            query = query.limit(numLimit)
+        return ChunkIterator(self, query, chunk_size)
