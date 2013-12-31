0488b5a0b7fbb8e69d84c0a1e7322ab757284993
Peter Yoachim <yoachim@uw.edu>
2013-12-31 12:01:01 -0800

    1st attempt at driver with pex config

---
+++ python/lsst/sims/operations/maf/driver/mafConfig.py
@@ -0,0 +1,85 @@
+import lsst.pex.config as pexConfig
+
+class MafConfig(pexConfig.Config):
+    """Using pexConfig to set MAF configuration parameters"""
+    dbAddress = pexConfig.Field("Address to the database to query." , str, '')
+    outputDir = pexConfig.Field("Location to write MAF output", str, '')
+    opsimNames = pexConfig.Field("Which opsim runs should be analyzed", str, 'opsim_3_61')
+    
+    # Manually unrolling loop since pex can't take a list of lists.
+    
+    grid1 = pexConfig.Field("", dtype=str, default='') # Change this to a choiceField? Or do we expect users to make new grids? 
+    kwrdsForGrid1 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
+    
+    grid2 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid2 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid3 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid3 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid4 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid4 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid5 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid5 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid6 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid6 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid7 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid7 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid8 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid8 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid9 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid9 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
+
+    grid10 = pexConfig.Field("", dtype=str, default='')
+    kwrdsForGrid10 = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
+
+    
+    # Up to 10 possible grids?  Is that more than enough?
+
+
+    
---
+++ python/lsst/sims/operations/maf/driver/mafDriver.py
@@ -0,0 +1,63 @@
+import numpy as np #segfault if numpy not imported 1st, argle bargle!
+from mafConfig import MafConfig
+import lsst.sims.operations.maf.grids as grids
+import lsst.sims.operations.maf.metrics as metrics
+
+def MafDriver(configOverrideFilename=None):
+    """Script for configuring and running metrics on Opsim output """
+    config=MafConfig()
+
+    # Load any config file
+    if configOverrideFilename is not None:
+        config.load(configOverrideFilename)
+
+    # Load any parameters set on the command line
+    
+    # Validate and freeze the config
+    config.validate()
+    config.freeze()
+
+    # Construct the grid and metric objects
+    s_grids = [config.grid1,config.grid2,config.grid3,config.grid4,config.grid5,
+               config.grid6,config.grid7,config.grid8,config.grid9,config.grid10]
+    s_gridKwrds = [config.kwrdsForGrid1,config.kwrdsForGrid2,config.
+                   kwrdsForGrid3,config.kwrdsForGrid4,config.kwrdsForGrid5,
+                   config.kwrdsForGrid6,config.kwrdsForGrid7,config.kwrdsForGrid8,
+                   config.kwrdsForGrid9,config.kwrdsForGrid10 ]
+    s_metrics = [config.metricsForGrid1,config.metricsForGrid2, config.metricsForGrid3,
+                 config.metricsForGrid4, config.metricsForGrid5, config.metricsForGrid6,
+                 config.metricsForGrid7, config.metricsForGrid8, config.metricsForGrid9,
+                 config.metricsForGrid10]
+    s_metricParams = [config.metricParamsForGrid1,config.metricParamsForGrid2
+                      ,config.metricParamsForGrid3,config.metricParamsForGrid4,config.metricParamsForGrid5,
+                      config.metricParamsForGrid6,config.metricParamsForGrid7,config.metricParamsForGrid8,config.metricParamsForGrid9
+                      ,config.metricParamsForGrid10]
+    s_metricKwrds = [config.metricKwrdsForGrid1,config.metricKwrdsForGrid2,config.metricKwrdsForGrid3 ]
+    constraints = [config.constraintsForGrid1,config.constraintsForGrid2,config.constraintsForGrid3]
+
+    gridList=[]
+    metricList=[]
+    
+    for i,s_grid in enumerate(s_grids):
+        if s_grid is not '':
+            gridList.append(getattr(grids,s_grid)(**eval('dict('+ s_gridKwrds[i]+')'))   )
+            sub_metricList=[]
+            for j,s_metric in enumerate(s_metrics[i]):
+                sub_metricList.append(getattr(metrics,s_metric)(*s_metricParams[i][j].split(','), **eval('dict('+s_metricKwrds[i][j]+')' )))
+            metricList.append(sub_metricList)
+
+    
+    # Loop over each grid in gridList:  for i,grid in enumerate(gridList)
+        # Loop over each SQL-constraint for that grid:  for j,constr in enumerate(constraints[i])
+            # Pull data from db using constraints[i][j]
+            # build kd-tree if needed--maybe make ra,dec keys and leafsize keywords for HealpixGrid so easier to pass in.
+            # make gridmetric object
+            # gridmetric.setGrid(gridList[i])
+            # gridmetric.runGrid(metricList[i], data, yadda yadda)
+            # gridmetric.reduceAll() ; gridmetric.plotAll(), gridmetric.writeAll()
+
+if __name__ == "__main__":
+    import sys
+    configOverrideFilename = sys.argv[1]
+    MafDriver(configOverrideFilename)
+    
