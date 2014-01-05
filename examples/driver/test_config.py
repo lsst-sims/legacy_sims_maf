cb50c3cb0b89f88f8421364db4a621619b584a68
Peter Yoachim <yoachim@uw.edu>
2014-01-05 08:15:12 -0800

    making gridconfig

--- examples/driver/test_config.py
+++ examples/driver/test_config.py
@@ -1,21 +1,29 @@
+import numpy as np
+from lsst.sims.operations.maf.driver.mafConfig import GridPackConfig
+
 root.outputDir = './output'
 root.dbAddress ='mssql+pymssql://LSST-2:L$$TUser@fatboy.npl.washington.edu:1433/LSST'
 root.opsimNames = ['output_opsim3_61_forLynne']
 
 constraints = ["filter=\'r\'", "filter=\'r\' and night < 51000", "filter=\'i\' "]
 
+g = GridPackConfig()
+g.grid = 'GlobalGrid'
+g.kwrdsForGrid =  ''
+g.metricsForGrid = ['MeanMetric', 'RmsMetric','MaxMetric', 'MinMetric']
+g.metricParamsForGrid = ['5sigma_modified','seeing', '5sigma_modified','night' ]
+g.metricKwrdsForGrid = ['']*4
+g.constraintsForGrid = constraints
 
-root.grid1 ='GlobalGrid'
-root.kwrdsForGrid1 = ''
-root.metricsForGrid1 =['MeanMetric', 'RmsMetric','MaxMetric', 'MinMetric']
-root.metricParamsForGrid1 = ['5sigma_modified','seeing', '5sigma_modified','night' ]
-root.metricKwrdsForGrid1 = ['']*4
-root.constraintsForGrid1 = constraints
+root.grid1 = g
 
+g = GridPackConfig()
+g.grid = 'HealpixGrid'
+g.kwrdsForGrid = 'nside=128'
+g.metricsForGrid = ['Coaddm5Metric', 'MeanMetric', 'MinMetric']#, 'VisitPairsMetric']
+g.metricParamsForGrid =['5sigma_modified', 'seeing','night']#, '']
+g.metricKwrdsForGrid = ['']*3#, 'deltaTmin=15.0/60.0/24.0, deltaTmax=90.0/60.0/24.0']
+g.constraintsForGrid = constraints
 
-root.grid2 = 'HealpixGrid'
-root.kwrdsForGrid2 = 'nside=128'
-root.metricsForGrid2 = ['Coaddm5Metric', 'MeanMetric', 'MinMetric']#, 'VisitPairsMetric']
-root.metricParamsForGrid2 =['5sigma_modified', 'seeing','night']#, '']
-root.metricKwrdsForGrid2 = ['']*3#, 'deltaTmin=15.0/60.0/24.0, deltaTmax=90.0/60.0/24.0']
-root.constraintsForGrid2 = constraints
+root.grid2 = g
+
--- python/lsst/sims/operations/maf/driver/mafConfig.py
+++ python/lsst/sims/operations/maf/driver/mafConfig.py
@@ -1,4 +1,14 @@
+import numpy as np
 import lsst.pex.config as pexConfig
+
+class GridPackConfig(pexConfig.Config):
+    """Config for packaging up a grid and list of metrics to run together """
+    grid = pexConfig.Field("", dtype=str, default='') # Change this to a choiceField? Or do we expect users to make new grids? 
+    kwrdsForGrid = pexConfig.Field("", dtype=str, default='')
+    metricsForGrid = pexConfig.ListField("", dtype=str, default=[''])
+    constraintsForGrid = pexConfig.ListField("", dtype=str, default=[''])
+    metricParamsForGrid = pexConfig.ListField("", dtype=str, default=[''])
+    metricKwrdsForGrid = pexConfig.ListField("", dtype=str, default=[''])
 
 class MafConfig(pexConfig.Config):
     """Using pexConfig to set MAF configuration parameters"""
@@ -10,79 +20,17 @@ class MafConfig(pexConfig.Config):
     spatialKey2 =pexConfig.Field("first key to build KD tree on", str, 'fieldDec')
     leafsize = pexConfig.Field("leafsize for KD tree", float, 5000)
     # Manually unrolling loop since pex can't take a list of lists.
-    
-    grid1 = pexConfig.Field("", dtype=str, default='') # Change this to a choiceField? Or do we expect users to make new grids? 
-    kwrdsForGrid1 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid1 = pexConfig.ListField("", dtype=str, default=[''])
-    
-    grid2 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid2 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid2 = pexConfig.ListField("", dtype=str, default=[''])
 
-    grid3 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid3 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid3 = pexConfig.ListField("", dtype=str, default=[''])
-
-    grid4 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid4 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid4 = pexConfig.ListField("", dtype=str, default=[''])
-
-    grid5 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid5 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid5 = pexConfig.ListField("", dtype=str, default=[''])
-
-    grid6 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid6 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid6 = pexConfig.ListField("", dtype=str, default=[''])
-
-    grid7 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid7 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid7 = pexConfig.ListField("", dtype=str, default=[''])
-
-    grid8 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid8 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid8 = pexConfig.ListField("", dtype=str, default=[''])
-
-    grid9 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid9 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid9 = pexConfig.ListField("", dtype=str, default=[''])
-
-    grid10 = pexConfig.Field("", dtype=str, default='')
-    kwrdsForGrid10 = pexConfig.Field("", dtype=str, default='')
-    metricsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
-    constraintsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
-    metricParamsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
-    metricKwrdsForGrid10 = pexConfig.ListField("", dtype=str, default=[''])
-
-    
-    # Up to 10 possible grids?  Is that more than enough?
+    grid1 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid2 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid3 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid4 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid5 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid6 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid7 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid8 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid9 = pexConfig.ConfigField("",GridPackConfig, default=None)
+    grid10 = pexConfig.ConfigField("",GridPackConfig, default=None)
 
 
     
