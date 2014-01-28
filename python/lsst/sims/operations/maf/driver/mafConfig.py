035d9f1388b00418eef709b66a87363a00f1797f
Peter Yoachim <yoachim@uw.edu>
2014-01-27 16:19:00 -0800

    started implamenting new config setup

--- python/lsst/sims/operations/maf/driver/mafConfig.py
+++ python/lsst/sims/operations/maf/driver/mafConfig.py
@@ -1,6 +1,42 @@
 import numpy as np
 import lsst.pex.config as pexConfig
 
+
+class MetricConfig(pexConfig.Config):
+    metric = pexConfig.Field("", dtype=str, default='')
+    kwargs = pexConfig.Field("", dtype=str, default='')
+    params = pexConfig.Field("", dtype=str, default='')
+
+class BinnerConfig(pexConfig.Config):
+    binner = pexConfig.Field("", dtype=str, default='') # Change this to a choiceField? Or do we expect users to make new bins?
+    kwargs =  pexConfig.Field("", dtype=str, default='')
+    params =  pexConfig.Field("", dtype=str, default='')
+    metricDict = pexConfig.ConfigDictField(doc="dict of index: metric config", keytype=int, itemtype=MetricConfig, default={})
+
+class MafConfig(pexConfig.Config):
+    """Using pexConfig to set MAF configuration parameters"""
+    dbAddress = pexConfig.Field("Address to the database to query." , str, '')
+    outputDir = pexConfig.Field("Location to write MAF output", str, '')
+    opsimNames = pexConfig.ListField("Which opsim runs should be analyzed", str, ['opsim_3_61'])
+    constraints = pexConfig.ListField("", dtype=str, default=[''])
+    binners = pexConfig.ConfigDictField(doc="dict of index: binner config", keytype=int, itemtype=BinnerConfig, default={}) 
+
+def makeDict(*args):
+    """Make a dict of index: config from a list of configs
+    """
+    return dict((ind, config) for ind, config in enumerate(args))
+
+def makeMetricConfig(name, params='', kwargs=''):
+    mc = MetricConfig()
+    mc.metric = name
+    mc.kwargs=kwargs
+    mc.params=params
+    return mc
+
+ 
+                                        
+
+                                      
 class BinPackConfig(pexConfig.Config):
     """Config for packaging up a grid and list of metrics to run together """
     binner = pexConfig.Field("", dtype=str, default='') # Change this to a choiceField? Or do we expect users to make new bins? 
