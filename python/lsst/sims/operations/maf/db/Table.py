194ed26a17e2a7d7324af70d82948e18d7c7d2a2
Simon Krughoff <krughoff@uw.edu>
2014-01-27 16:45:19 -0800

    Changes to Table.py: 1. Use the column object in the group by to preserve correct table references instead of the string column name which makes SQLite complain 2. Fix the aggregation to have a default (func.min) and to leave the aggregate out if there is no group by.

--- python/lsst/sims/operations/maf/db/Table.py
+++ python/lsst/sims/operations/maf/db/Table.py
@@ -33,7 +33,7 @@ class Table(DBObject):
         self._make_column_map()
         self._make_type_map()
 
-    def _get_column_query(self, colnames=None):
+    def _get_column_query(self, doGroupBy, colnames=None, aggregate=func.min):
         # Build the sql query - including adding all column names, if columns were None.
         if colnames is None:
             colnames = [k for k in self.columnMap.keys()]
@@ -55,10 +55,13 @@ class Table(DBObject):
             idLabel = idColName
 
         #SQL server requires an aggregate on all columns if a group by clause is used.
-        #Modify the columnMap to take care of this.  I'm choosing MIN, but it shouldn't
+        #Modify the columnMap to take care of this.  The default is MIN, but it shouldn't
         #matter since the entries are almost identical (except for proposalId???)
         #Added double-quotes to handle column names that start with a number.
-        query = self.session.query(func.min(self.table.c[idColName]).label(idLabel))
+        if doGroupBy:
+            query = self.session.query(aggregate(self.table.c[idColName]).label(idLabel))
+        else:
+            query = self.session.query(self.table.c[idColName].label(idLabel))
         for col, val in zip(colnames, vals):
             if val is idColName:
                 continue
@@ -66,24 +69,27 @@ class Table(DBObject):
             if val == col:
                 #If so, use the column in the table to take care of DB specific column
                 #naming conventions (capitalization, spaces, etc.)
-                query = query.add_column(func.min(self.table.c[col]).label(col))
+                if doGroupBy:
+                    query = query.add_column(aggregate(self.table.c[col]).label(col))
+                else:
+                    query = query.add_column(self.table.c[col].label(col))
             else:
                 #If not, assume that the user has specified the column correctly
-                query = query.add_column(func.min(expression.literal_column(val)).label(col))
+                if doGroupBy:
+                    query = query.add_column(aggregate(expression.literal_column(val)).label(col))
+                else:
+                    query = query.add_column(expression.literal_column(val).label(col))
         return query
 
     def query_columns(self, colnames=None, chunk_size=None, constraint=None, groupByCol=None, numLimit=None):
-
-        query = self._get_column_query(colnames)
+        doGroupBy = not groupByCol is None
+        query = self._get_column_query(doGroupBy, colnames=colnames)
         if constraint is not None:
             query = query.filter(constraint)
 
-        if groupByCol is not None:
+        if doGroupBy:
             #Either group by a column that gives unique visits
-            query = query.group_by(groupByCol)
-        #else:
-            #or group by the unique id for the table.
-        #    query = query.group_by(self.idColKey)
+            query = query.group_by(self.table.c[groupByCol])
         if numLimit:
             query = query.limit(numLimit)
         return ChunkIterator(self, query, chunk_size)
