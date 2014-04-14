a2db253a446d80500f2f75731d84fa345046ac93
Peter Yoachim <yoachim@uw.edu>
2014-04-14 14:25:33 -0700

    switched runDriver to argparse

--- bin/runDriver.py
+++ bin/runDriver.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python
 import lsst.sims.operations.maf.driver as driver
-import sys
+import sys, os, argparse
 
 
 if __name__=="__main__":
-    filename = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ConfigFile", type=str, help="Name of the configuration file, a pex_config python script.")
+    args = parser.parse_args()
+    filename = args.ConfigFile
     drive = driver.MafDriver(filename)
     drive.run()
     
