f7ec2652ad0a8536aa20988a8dd4d7ee5bfd2881
Peter Yoachim <yoachim@Peters-MacBook-Pro.local>
2014-04-17 20:27:48 -0700

    removed cruft from __init__

--- python/lsst/sims/operations/maf/metrics/__init__.py
+++ python/lsst/sims/operations/maf/metrics/__init__.py
@@ -5,7 +5,6 @@ from .visitPairsMetric import *
 from .fftMetric import *
 from .properMotionMetric import *
 from .parallaxMetric import *
-from .observeEfficiencyMetric import *
 from .hourglassMetric import *
 from .completenessMetric import *
 from .radiusObsMetric import *
