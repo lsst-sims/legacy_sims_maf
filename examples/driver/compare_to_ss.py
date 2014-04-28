1f3ac859645607756e3f04c120acf27128757e00
Peter Yoachim <yoachim@uw.edu>
2014-04-28 13:53:08 -0700

    fiddling compare driver

--- examples/driver/compare_to_ss.py
+++ examples/driver/compare_to_ss.py
@@ -30,7 +30,7 @@ DDpropid = 94 #XXX Guess
 
 
 filters = ['u','g','r','i','z','y']
-colors={'u':'m','g':'b','r':'g','i':'y','z':'o','y':'r'}
+colors={'u':'m','g':'b','r':'g','i':'y','z':'Orange','y':'r'}
 #filters=['r']
 
 # 10 year Design Specs
@@ -38,6 +38,8 @@ nvisitBench={'u':56,'g':80, 'r':184, 'i':184, 'z':160, 'y':160}
 nVisits_plotRange = {'all': 
                      #{'u':[25, 75], 'g':[50,100], 'r':[150, 200], 'i':[150, 200], 'z':[100, 250], 'y':[100,250]},
                      {'u':[0, 200], 'g':[0,200], 'r':[0, 200], 'i':[0, 200], 'z':[0, 200], 'y':[0,200]},
+                     'visits':
+                         {'u':[0, 70], 'g':[0,100], 'r':[0, 250], 'i':[0, 250], 'z':[0, 200], 'y':[0,200]  }, 
                      'DDpropid': 
                      {'u':[6000, 10000], 'g':[2500, 5000], 'r':[5000, 8000], 'i':[5000, 8000],  'z':[7000, 10000], 'y':[5000, 8000]},
                      '216':
@@ -54,11 +56,15 @@ for i,f in enumerate(filters):
     m1 = makeMetricConfig('CountMetric', params=['expMJD'], kwargs={'metricName':'Nvisits'}, 
                           plotDict={'units':'Number of Visits', 
                                     'histMin':nVisits_plotRange['all'][f][0],
-                                    'histMax':nVisits_plotRange['all'][f][1]})
+                                    'histMax':nVisits_plotRange['all'][f][1]
+                                    'plotMin':nVisits_plotRange['visits'][f][0], 'plotMax':nVisits_plotRange['visits'][f][1]})
     m2 = makeMetricConfig('CountMetric', params=['expMJD'], kwargs={'metricName':'NVisitsRatio'},
-                          plotDict={'normVal':nvisitBench[f], 'plotMin':0.5, 'plotMax':1.2, 'ylog':False, 'units':'Number of Visits/Benchmark (%d)' %(nvisitBench[f])})
+                          plotDict={'normVal':nvisitBench[f], 'plotMin':0.5, 'plotMax':1.2,
+                                    'ylog':False, 'units':'Number of Visits/Benchmark (%d)' %(nvisitBench[f])})
     m3 = makeMetricConfig('MedianMetric', params=['5sigma'])
-    m4 = makeMetricConfig('Coaddm5Metric', plotDict={'zp':mag_zpoints[f], 'percentileClip':95., 'units':'Co-add (m5 - %.1f)'%mag_zpoints[f], 'histMin':23, 'histMax':28},
+    m4 = makeMetricConfig('Coaddm5Metric', kwargs={'m5col':'5sigma'},
+                          plotDict={'zp':mag_zpoints[f], 'plotMin':-0.8, 'plotMax':0.8,
+                                    'units':'Co-add (m5 - %.1f)'%mag_zpoints[f], 'histMin':23, 'histMax':28},
                           histMerge={'histNum':6, 'legendloc':'upper right', 'color':colors[f],'label':'%s'%f} )             
     m5 = makeMetricConfig('MedianMetric', params=['perry_skybrightness'], plotDict={'zp':sky_zpoints[f], 'units':'Skybrightness - %.2f' %(sky_zpoints[f])})
     m6 = makeMetricConfig('MedianMetric', params=['finSeeing'], plotDict={'normVal':seeing_norm[f], 'units':'Median Seeing/(Expected seeing %.2f)'%(seeing_norm[f])})
@@ -77,7 +83,10 @@ for i,f in enumerate(filters):
     m2 = makeMetricConfig('CountMetric', params=['expMJD'], kwargs={'metricName':'NVisitsRatio'},
                           plotDict={'normVal':nvisitBench[f], 'percentileClip':80., 'units':'Number of Visits/Benchmark (%d)' %(nvisitBench[f])})
     m3 = makeMetricConfig('MedianMetric', params=['5sigma'])
-    m4 = makeMetricConfig('Coaddm5Metric', plotDict={'zp':mag_zpoints[f], 'percentileClip':95., 'units':'Co-add (m5 - %.1f)'%mag_zpoints[f], 'histMin':23, 'histMax':28})             
+    m4 = makeMetricConfig('Coaddm5Metric', kwargs={'m5col':'5sigma', metricName='Coaddm5WFD'},
+                          plotDict={'zp':mag_zpoints[f], 'plotMin':-0.8, 'plotMax':0.8, 'percentileClip':95.,
+                                    'units':'Co-add (m5 - %.1f)'%mag_zpoints[f], 'histMin':23, 'histMax':28}
+                           histMerge={'histNum':7, 'legendloc':'upper right', 'color':colors[f],'label':'%s'%f})             
     m5 = makeMetricConfig('MedianMetric', params=['perry_skybrightness'], plotDict={'zp':sky_zpoints[f], 'units':'Skybrightness - %.2f' %(sky_zpoints[f])})
     m6 = makeMetricConfig('MedianMetric', params=['finSeeing'], plotDict={'normVal':seeing_norm[f], 'units':'Median Seeing/(Expected seeing %.2f)'%(seeing_norm[f])})
     m7 = makeMetricConfig('MedianMetric', params=['airmass'], plotDict={'_unit':'X'})
@@ -127,7 +136,7 @@ binList.append(binner)
 
 # The merged histograms for basics 
 for i,f in enumerate(filters):
-    m1 = makeMetricConfig('CountMetric', params=['5sigma'], plotDict={'histMin':20, 'histMax':26}
+    m1 = makeMetricConfig('CountMetric', params=['5sigma'], plotDict={'histMin':20, 'histMax':26},
                           histMerge={'histNum':1, 'legendloc':'upper right', 'color':colors[f],'label':'%s'%f} )
     binner = makeBinnerConfig('OneDBinner', kwargs={"sliceDataColName":'5sigma'},
                               metricDict=makeDict(m1), constraints=["filter = '%s'and propID = %i"%(f,WFDpropid)]) 
