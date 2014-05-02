d0ca2a8971111e33deeafc13eb0ce5adb5b6423a
Lynne Jones <ljones.uw@gmail.com>
2014-05-02 13:39:57 -0700

    Unit test complete up to plotting, and baseBinMetric has been slightly updated (picked up one of Peter's changes from another branch where he removed unibinner special case from summaryMetric and discovered/fixed a bug in the masking during runBins).

--- tests/testBaseBinMetric.py
+++ tests/testBaseBinMetric.py
@@ -13,22 +13,30 @@ def makeDataValues(size=100, min=0., max=1., random=True):
         randorder = np.random.rand(size)        
         randind = np.argsort(randorder)
         datavalues = datavalues[randind]
-    datavalues = np.array(zip(datavalues), dtype=[('testdata', 'float')])
+    filtervalues = np.empty(size, dtype='str')
+    half = int(size/2)
+    filtervalues[0:half] = 'g'
+    filtervalues[half:size] = 'r'
+    datavalues = np.core.records.fromarrays([datavalues, filtervalues], names=['testdata', 'filter'])
     return datavalues
+
 
 class TestSetupBaseBinMetric(unittest.TestCase):
     """Unit tests relating to setting up the baseBinMetric"""
     def setUp(self):
         self.testbbm = binMetrics.BaseBinMetric()
-        self.m1 = metrics.MeanMetric('testdata', plotParams={'units':'meanunits'})
-        self.m2 = metrics.CountMetric('testdata', plotParams={'units':'countunits',
-                                                            'title':'count_title'})
+        self.m1 = metrics.MeanMetric('testdata', metricName='Mean testdata',
+                                     plotParams={'units':'meanunits'})
+        self.m2 = metrics.CountMetric('testdata', metricName='Count testdata',
+                                      plotParams={'units':'countunits', 'title':'count_title'})
+        self.m3 = metrics.CompletenessMetric('filter', metricName='Completeness', g=50, r=50)
         self.binner = binners.UniBinner()
 
     def tearDown(self):
         del self.testbbm
         del self.m1
         del self.m2
+        del self.m3
         del self.binner
         self.testbbm = None
         self.m1 = None
@@ -71,14 +79,14 @@ class TestSetupBaseBinMetric(unittest.TestCase):
 
     def testSetMetrics(self):
         """Test setting metrics and de-dupe/dupe metric names."""
-        self.testbbm.setMetrics([self.m1, self.m2])
+        self.testbbm.setMetrics([self.m1, self.m2, self.m3])
         # Test metricNames list is as expected.
-        self.assertEqual(self.testbbm.metricNames, ['Mean testdata', 'Count testdata'])
+        self.assertEqual(self.testbbm.metricNames, ['Mean testdata', 'Count testdata', 'Completeness'])
         # Test that dictionaries for metricObjs (which hold metric python objects) set
-        self.assertEqual(self.testbbm.metricObjs.keys(), ['Mean testdata', 'Count testdata'])
-        self.assertEqual(self.testbbm.metricObjs.values(), [self.m1, self.m2])
+        self.assertEqual(self.testbbm.metricObjs.keys(), ['Mean testdata', 'Count testdata', 'Completeness'])
+        self.assertEqual(self.testbbm.metricObjs.values(), [self.m1, self.m2, self.m3])
         # Test that plot parameters were passed through as expected
-        self.assertEqual(self.testbbm.plotParams.keys(), ['Mean testdata', 'Count testdata'])
+        self.assertEqual(self.testbbm.plotParams.keys(), ['Mean testdata', 'Count testdata', 'Completeness'])
         self.assertEqual(self.testbbm.plotParams['Mean testdata'].keys(), ['units', '_unit'])
         self.assertEqual(self.testbbm.plotParams['Count testdata'].keys(), ['units', '_unit', 'title'])
         self.assertEqual(self.testbbm.plotParams['Count testdata'].values(),
@@ -88,8 +96,8 @@ class TestSetupBaseBinMetric(unittest.TestCase):
         testbbm2.setMetrics(self.m1)
         self.assertEqual(testbbm2.metricNames, ['Mean testdata',])
         # Test that if add an additional metric, the name is 'de-duped' as expected (and added)
-        m3 = metrics.MeanMetric('testdata')
-        testbbm2.setMetrics(m3)
+        m4 = metrics.MeanMetric('testdata')
+        testbbm2.setMetrics(m4)
         self.assertEqual(testbbm2.metricNames, ['Mean testdata', 'Mean testdata__0'])
         # And that we can de-dupe name as expected.
         self.assertEqual(testbbm2._dupeMetricName('Mean testdata__0'), 'Mean testdata')
@@ -97,26 +105,37 @@ class TestSetupBaseBinMetric(unittest.TestCase):
     def testValidateMetricData(self):
         """Test validation of metric data."""
         dv = makeDataValues()
-        self.testbbm.setMetrics([self.m1, self.m2])
+        # Test that validates correctly if all columns present in data. 
+        self.testbbm.setMetrics([self.m1, self.m2, self.m3])
         self.assertTrue(self.testbbm.validateMetricData(dv))
-
+        # Test that raises exception if not all columns present in data.
+        m4 = metrics.MeanMetric('notTestData')
+        self.testbbm.setMetrics(m4)
+        self.assertRaises(Exception, self.testbbm.validateMetricData, dv)
 
 class TestRunBaseBinMetric(unittest.TestCase):        
     def setUp(self):
         self.testbbm = binMetrics.BaseBinMetric()
-        self.m1 = metrics.MeanMetric('testdata', plotParams={'units':'meanunits'})
-        self.m2 = metrics.CountMetric('testdata', plotParams={'units':'countunits',
-                                                            'title':'count_title'})
+        self.m1 = metrics.MeanMetric('testdata', metricName='Mean testdata',
+                                     plotParams={'units':'meanunits'})
+        self.m2 = metrics.CountMetric('testdata', metricName='Count testdata',
+                                      plotParams={'units':'countunits', 'title':'count_title'})
+        self.m3 = metrics.CompletenessMetric('filter', metricName='Completeness', g=500, r=500,
+                                             plotParams={'xlabel':'Completeness'})
+        self.metricNames = ['Mean testdata', 'Count testdata', 'Completeness']
+        self.reduceNames = ['Completeness_u', 'Completeness_g', 'Completeness_r', 'Completeness_i',
+                            'Completeness_z', 'Completeness_y', 'Completeness_Joint']
+        self.dv = makeDataValues(size=1000, min=0, max=1)
         self.binner = binners.OneDBinner('testdata')
-        self.dv = makeDataValues(size=1000)
-        self.binner.setupBinner(self.dv)
+        self.binner.setupBinner(self.dv, bins=np.arange(0, 1.25, .1))
         self.testbbm.setBinner(self.binner)
-        self.testbbm.setMetrics([self.m1, self.m2])
+        self.testbbm.setMetrics([self.m1, self.m2, self.m3])
 
     def tearDown(self):
         del self.testbbm
         del self.m1
         del self.m2
+        del self.m3
         del self.binner
         self.testbbm = None
         self.m1 = None
@@ -124,14 +143,239 @@ class TestRunBaseBinMetric(unittest.TestCase):
         self.binner = None
 
     def testRunBins(self):
+        """Test creating metric data values."""
         opsimname = 'opsim1000'
         metadata = 'created fake testdata'
-        comment = 'created data'
+        comment = 'testing fake data run'
+        self.testbbm.runBins(self.dv, simDataName=opsimname, metadata=metadata, comment=comment)
+        # Test that copied opsim name and metadata and comment correctly for each metric name.
+        for mname in self.metricNames:
+            self.assertEqual(self.testbbm.simDataName[mname], opsimname)
+            self.assertEqual(self.testbbm.metadata[mname], metadata)
+            self.assertEqual(self.testbbm.comment[mname], comment)
+        # Test that created metric data with expected number of data points.
+        for mname in self.metricNames:
+            self.assertEqual(len(self.testbbm.metricValues[mname]), len(self.binner))
+        # Test that metric data was masked where expected (last bin) due to no data in bin.
+        lastbin = len(self.binner) - 1
+        for mname in self.metricNames:
+            self.assertEqual(self.testbbm.metricValues[mname].mask[lastbin], True)
+
+    def testReduce(self):
+        """Test running reduce methods."""
+        # Completeness metric has reduce methods, so check on those.
+        opsimname = 'opsim1000'
+        metadata = 'created fake testdata'
+        comment = 'testing fake data run'
+        self.testbbm.runBins(self.dv, simDataName=opsimname, metadata=metadata, comment=comment)
+        self.testbbm.reduceAll()
+        # Check that all metric data values expected exist.
+        for m in self.metricNames:
+            self.assertTrue(m in self.testbbm.metricValues.keys())
+        for m in self.reduceNames:
+            self.assertTrue(m in self.testbbm.metricValues.keys())
+        # Check that simdata, metadata and comments were copied properly.
+        for m in self.reduceNames:
+            self.assertEqual(self.testbbm.simDataName[m], opsimname)
+            self.assertEqual(self.testbbm.metadata[m], metadata)
+            self.assertEqual(self.testbbm.comment[m], comment)
+        # Check that plot parameters were copied properly.
+        for m in self.reduceNames:
+            self.assertEqual(self.testbbm.plotParams[m]['xlabel'], 'Completeness')
+        # Check that mask carried through properly.
+        lastbin = len(self.binner) - 1
+        for m in self.reduceNames:
+            self.assertEqual(self.testbbm.metricValues[m].mask[lastbin], True)
+                
+
+class TestReadWriteBaseBinMetric(unittest.TestCase):        
+    def setUp(self):
+        self.testbbm = binMetrics.BaseBinMetric()
+        self.m1 = metrics.MeanMetric('testdata', metricName='Mean testdata',
+                                     plotParams={'units':'meanunits'})
+        self.m2 = metrics.CountMetric('testdata', metricName='Count testdata',
+                                      plotParams={'units':'countunits', 'title':'count_title'})
+        self.m3 = metrics.CompletenessMetric('filter', metricName='Completeness', g=500, r=500,
+                                             plotParams={'xlabel':'Completeness'})
+        self.metricNames = ['Mean testdata', 'Count testdata', 'Completeness']
+        self.reduceNames = ['Completeness_u', 'Completeness_g', 'Completeness_r', 'Completeness_i',
+                            'Completeness_z', 'Completeness_y', 'Completeness_Joint']
+        self.dv = makeDataValues(size=1000, min=0, max=1)
+        self.binner = binners.OneDBinner('testdata')
+        self.binner.setupBinner(self.dv, bins=np.arange(0, 1.25, .1))
+        self.testbbm.setBinner(self.binner)
+        self.testbbm.setMetrics([self.m1, self.m2, self.m3])
+        self.opsimname = 'opsim1000'
+        self.metadata = 'created fake testdata'
+        self.comment = 'testing fake data run'
+        self.testbbm.runBins(self.dv, simDataName=self.opsimname, metadata=self.metadata, comment=self.comment)
+        self.testbbm.reduceAll()
+        self.outroot = 'testBaseBinMetric'
+        self.testbbm.writeAll(outDir='.', outfileRoot=self.outroot)
+        self.expectedfiles = []
+        for m in self.metricNames:
+            filename = (self.outroot + ' ' + m + ' ' + self.comment + ' ' +
+                        self.binner.binnerName[:4].upper() + '.npz')
+            filename = filename.replace(' ', '_')
+            self.expectedfiles.append(filename)
+        for m in self.reduceNames:
+            filename = (self.outroot + ' ' + m + ' ' + self.comment + ' ' +
+                        self.binner.binnerName[:4].upper() + '.npz')
+            filename = filename.replace(' ', '_')
+            self.expectedfiles.append(filename)
+                        
+    def tearDown(self):
+        del self.testbbm
+        del self.m1
+        del self.m2
+        del self.m3
+        del self.binner
+        self.testbbm = None
+        self.m1 = None
+        self.m2 = None
+        self.binner = None
+        import os
+        for f in self.expectedfiles:
+            os.remove(f)        
+
+    def testWrite(self):
+        """Test writing data to disk (and test outfile name generation)."""
+        import os
+        filelist = os.listdir('.')
+        for f in self.expectedfiles:
+            self.assertTrue(f in filelist)
+
+    def testRead(self):
+        """Test reading data back from disk. """
+        # Test with binner already set up in binMetric.
+        filename = (self.outroot + '_' + 'Completeness' + '_' + self.comment + '_' +
+                    self.binner.binnerName[:4].upper() + '.npz')
+        filename = filename.replace(' ', '_')
+        self.testbbm.readMetricValues(filename)
+        # Should be read in and de-duped.
+        self.assertTrue('Completeness__0' in self.testbbm.metricValues)
+        for m, n in zip(self.testbbm.metricValues['Completeness'].data,
+                        self.testbbm.metricValues['Completeness__0'].data):
+            np.testing.assert_equal(m, n)
+        for m, n in zip(self.testbbm.metricValues['Completeness'].mask,
+                        self.testbbm.metricValues['Completeness__0'].mask):
+            self.assertEqual(m, n)
+        # Test with new binMetric (with no binner previously set up).
+        testbbm2 = binMetrics.BaseBinMetric()
+        testbbm2.readMetricValues(filename)
+        self.assertTrue('Completeness' in testbbm2.metricValues)
+        for m, n in zip(self.testbbm.metricValues['Completeness'].data,
+                        testbbm2.metricValues['Completeness'].data):
+            np.testing.assert_equal(m, n)
+        for m, n in zip(self.testbbm.metricValues['Completeness'].mask,
+                        testbbm2.metricValues['Completeness'].mask):
+            self.assertEqual(m, n)
+        # Check if simDataName and metadata were restored as expected.
+        self.assertEqual(testbbm2.simDataName['Completeness'], self.testbbm.simDataName['Completeness'])
+        self.assertEqual(testbbm2.metadata['Completeness'], self.testbbm.metadata['Completeness'])
+        # plot parameters not currently being written to disk
+        #self.assertEqual(testbbm2.plotParams['Completeness']['xlabel'], 'Completeness')
+
+    def testOutputFileKey(self):
+        """Test that the output file key is being generated as expected."""
+        outkeys = self.testbbm.returnOutputFiles(verbose=False)
+        # Check data in outkeys is valid
+        for o in outkeys:
+            self.assertEqual(o['comment'], self.comment)
+            self.assertEqual(o['simDataName'], self.opsimname)
+            self.assertEqual(o['metadata'], self.metadata)
+            self.assertTrue(o['filename'].replace('./', '') in self.expectedfiles)
+            self.assertEqual(o['binner'], self.binner.binnerName)
+            self.assertTrue((o['metricName'] in self.metricNames) or (o['metricName'] in self.reduceNames)) 
+        # Check data in outkeys is complete
+        outkeysMetricNames = []
+        for o in outkeys:
+            outkeysMetricNames.append(o['metricName'])
+        for m in self.metricNames:
+            self.assertTrue(m in outkeysMetricNames)
+        for m in self.reduceNames:
+            self.assertTrue(m in outkeysMetricNames)
+
+class TestSummaryStatisticBaseBinMetric(unittest.TestCase):
+    def setUp(self):
+        self.testbbm = binMetrics.BaseBinMetric()
+        self.m1 = metrics.MeanMetric('testdata', metricName='Mean testdata',
+                                     plotParams={'units':'meanunits'})
+        self.dv = makeDataValues(size=1000, min=0, max=1)
+        self.testbbm.setMetrics([self.m1,])
+        self.summaryStat = metrics.MeanMetric('metricData')
+        self.opsimname = 'opsim1000'
+        self.metadata = 'created fake testdata'
+        self.comment = 'testing fake data run'
         
+    def tearDown(self):
+        del self.testbbm
+        del self.m1
+        del self.binner
+        self.testbbm = None
+        self.m1 = None
+        self.binner = None
+
+    def testSummaryStatistic(self):
+        """Test summary statistic calculation."""
+        # Try unibinner first: expect that summary statistic return will be simply the unibinner value.
+        self.binner = binners.UniBinner()
+        self.binner.setupBinner(self.dv)
+        self.testbbm.setBinner(self.binner)
+        self.testbbm.runBins(self.dv, simDataName=self.opsimname, metadata=self.metadata, comment=self.comment)
+        summary = self.testbbm.computeSummaryStatistics('Mean testdata', self.summaryStat)
+        self.assertEqual(summary, self.testbbm.metricValues['Mean testdata'][0])
+        summary = self.testbbm.computeSummaryStatistics('Mean testdata', metrics.IdentityMetric('metricdata'))
+        self.assertEqual(summary, self.testbbm.metricValues['Mean testdata'][0])
+        # Try oneD binner
+        self.testbbm = binMetrics.BaseBinMetric()
+        self.testbbm.setMetrics([self.m1,])
+        self.binner = binners.OneDBinner('testdata')
+        self.binner.setupBinner(self.dv, nbins=100)
+        self.testbbm.setBinner(self.binner)
+        self.testbbm.runBins(self.dv, simDataName=self.opsimname, metadata=self.metadata, comment=self.comment)
+        summary = self.testbbm.computeSummaryStatistics('Mean testdata', self.summaryStat)
+        self.assertEqual(summary, self.testbbm.metricValues['Mean testdata'].mean())
+        # Other binners (spatial binner, etc) should be similar to oneD binner.
+
+            
+class TestPlottingBaseBinMetric(unittest.TestCase):
+    def setUp(self):
+        self.testbbm = binMetrics.BaseBinMetric()
+        self.m1 = metrics.MeanMetric('testdata', metricName='Mean testdata',
+                                     plotParams={'units':'meanunits'})
+        self.dv = makeDataValues(size=1000, min=0, max=1)
+
+        self.binner = binners.OneDBinner('testdata')
+        self.binner.setupBinner(self.dv, bins=np.arange(0, 1.25, .1))
+        self.testbbm.setBinner(self.binner)
+        self.testbbm.setMetrics([self.m1,])
+        self.opsimname = 'opsim1000'
+        self.metadata = 'created fake testdata'
+        self.comment = 'testing fake data run'
+        self.testbbm.runBins(self.dv, simDataName=self.opsimname, metadata=self.metadata, comment=self.comment)
+                        
+    def tearDown(self):
+        del self.testbbm
+        del self.m1
+        del self.binner
+        self.testbbm = None
+        self.m1 = None
+        self.binner = None
+
+    def testPlotting(self):
+        """Test plotting."""
+
         
-        
+                                                                    
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSetupBaseBinMetric)
     unittest.TextTestRunner(verbosity=2).run(suite)
-
-        
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRunBaseBinMetric)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestReadWriteBaseBinMetric)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSummaryStatisticBaseBinMetric)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPlottingBaseBinMetric)
+    unittest.TextTestRunner(verbosity=2).run(suite)        
