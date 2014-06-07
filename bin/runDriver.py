ed87ef0d2085860a855b535ef3a0931fd82bf1d3
Peter Yoachim <yoachim@uw.edu>
2014-06-06 21:43:47 -0700

    made runDriver more general and moved intstall_*test.py to driverFuncs.py
    Former-commit-id: 78c3ac64986034bbb3711f1fd615242356c1af95

--- bin/runDriver.py
+++ bin/runDriver.py
@@ -4,15 +4,22 @@ matplotlib.use('Agg') # May want to change in the future if we want to display p
 import lsst.sims.maf.driver as driver
 import argparse
 from lsst.sims.maf.driver.mafConfig import MafConfig
+import lsst.sims.maf.driver.driverFuncs as driverFuncs
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("ConfigFile", type=str, help="Name of the configuration file, a pex_config python script.")
+    parser.add_argument("ConfigFile", type=str, help="Name of the configuration file(a pex_config python script) OR the function name from lsst.sims.maf.driver.driverFuncs that will build the config.")
+    parser.add_argument("--runName", type=str, default='', help='Root name of the sqlite dbfile (i.e. filename minus _sqlite.db)')
+    parser.add_argument("--filepath", type=str, default='.', help='Filepath to the sqlite dbfile')
+    parser.add_argument("--outputDir", type=str, default='./Out', help='Output directory')
+
     args = parser.parse_args()
-    filename = args.ConfigFile
     config = MafConfig()
-    config.load(filename)
-    print 'Finished loading config file: %s'%filename
+    if args.runName == '':
+        config.load(args.ConfigFile)
+        print 'Finished loading config file: %s'%args.ConfigFile
+    else:
+        config = getattr(driverFuncs,args.ConfigFile)(config, args.runName, args.filepath, args.outputDir)
+        print 'Finished loading config function %s'%args.ConfigFile
     drive = driver.MafDriver(config)
     drive.run()
-    
--- python/lsst/sims/maf/driver/driverFuncs.py
+++ python/lsst/sims/maf/driver/driverFuncs.py
@@ -1,16 +1,44 @@
-# A MAF config that replicates the SSTAR plots
-
-import sys, os, argparse
-# Need to set matplotlib backend here.
-import matplotlib
-matplotlib.use('Agg')
-
 from lsst.sims.maf.driver.mafConfig import MafConfig, makeBinnerConfig, makeMetricConfig, makeDict
 import lsst.sims.maf.utils as utils
-import lsst.sims.maf.driver as driver
+import os
+
+def install_initialtest(config, runName, filepath, outputDir):
+    """Set up the config values."""    
+
+    # Setup Database access (user does not need to edit)
+    config.outputDir = outputDir
+    sqlitefile = os.path.join(filepath, runName + '_sqlite.db')
+    config.dbAddress ={'dbAddress':'sqlite:///'+sqlitefile}
+    config.opsimName = runName
 
 
-def configSetup(config, runName, filepath, outputDir, binnertype='OpsimFieldBinner'):
+    ### User edit below here to define binner and metrics desired.
+    
+    binList=[]
+    
+    filters = ['r', 'g']
+    nside = 64
+
+    for f in filters:
+        m1 = makeMetricConfig('CountMetric', params=['expMJD'], kwargs={'metricName':'NVisits'}, 
+                            plotDict={'plotMin':0, 'plotMax':200, 'units':'N Visits'},
+                            summaryStats={'MeanMetric':{}, 'RmsMetric':{}})
+        m2 = makeMetricConfig('Coaddm5Metric', kwargs={'m5col':'fivesigma_modified'}, 
+                            plotDict={'percentileClip':95}, summaryStats={'MeanMetric':{}})
+        metricDict = makeDict(m1, m2)
+        constraint = 'filter = "%s"' %(f)
+        binner = makeBinnerConfig('HealpixBinner',
+                                  kwargs={'nside':nside, 'spatialkey1':'fieldRA', 'spatialkey2':'fieldDec'},
+                                metricDict=metricDict, constraints=[constraint,])
+        config.binners=makeDict(binner)
+        binList.append(binner)
+
+    ### End of user edit for simple configs.
+    config.binners=makeDict(*binList)
+    return config
+
+
+def install_biggertest(config, runName, filepath, outputDir, binnertype='OpsimFieldBinner'):
     """Set up the config values."""    
 
     # Setup Database access
@@ -257,20 +285,4 @@ def configSetup(config, runName, filepath, outputDir, binnertype='OpsimFieldBinn
 
     config.binners=makeDict(*binList)
     return config
-
-if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("runName", type=str, help='Root name of the sqlite dbfile (i.e. filename minus _sqlite.db)')
-    parser.add_argument("--filepath", type=str, default='.', help='Filepath to the sqlite dbfile')
-    parser.add_argument("--outputDir", type=str, default='./Out', help='Output directory')
-    parser.add_argument("--binnertype", type=str, default='OpsimFieldBinner',
-                        help='Choose a binnertype for metrics evaluated over the sky (HealpixBinner, OpsimFieldBinner, or HealpixBinnerDithered)')
-    args = parser.parse_args()
-
-    config = MafConfig()
-    config = configSetup(config, args.runName, args.filepath, args.outputDir, args.binnertype)
-    
-    drive = driver.MafDriver(config)
-    drive.run()
-
 
