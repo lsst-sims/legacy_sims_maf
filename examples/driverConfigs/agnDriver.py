f816312ae368d3d79f6540b53e7b43a5eafa0057
Peter Yoachim <yoachim@uw.edu>
2014-06-09 11:31:24 -0700

    blah
    Former-commit-id: c0f379cb27f25e0ee46f92094351de7ba7736d76

--- examples/driverConfigs/agnDriver.py
+++ examples/driverConfigs/agnDriver.py
@@ -1,10 +1,12 @@
 from lsst.sims.maf.driver.mafConfig import makeBinnerConfig, makeMetricConfig, makeDict
+import healpy as hp
+
 
 root.outputDir = './AGN'
 root.dbAddress ={'dbAddress':'sqlite:///../../tests/opsimblitz1_1131_sqlite.db'}
 root.opsimName = 'opsimblitz1_1131'
  
-m1 = makeMetricConfig('LongGapAGNMetric')
-binner = makeBinnerConfig('OpsimFieldBinner', metricDict=makeDict(m1), constraints=[''] )
+m1 = makeMetricConfig('LongGapAGNMetric', kwargs={'badval':hp.UNSEEN})
+binner = makeBinnerConfig('HealpixBinner', metricDict=makeDict(m1), constraints=[''] )
 root.binners=makeDict(binner)
 
