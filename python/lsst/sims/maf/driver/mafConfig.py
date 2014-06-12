b9370b2c9bc55d173ae0d2a7b2c7d9cd9ed2d52b
Peter Yoachim <yoachim@uw.edu>
2014-06-12 16:26:34 -0700

    started fixing config
    Former-commit-id: f3711be7fc497f2309eb44b214336f0c0a092772

--- python/lsst/sims/maf/driver/mafConfig.py
+++ python/lsst/sims/maf/driver/mafConfig.py
@@ -1,68 +1,59 @@
 import lsst.pex.config as pexConfig
-   
-class PlotConfig(pexConfig.Config):
+import numpy as np
+
+class MixConfig(pexConfig.Config):
     plot_str = pexConfig.DictField("", keytype=str, itemtype=str, default={})
     plot_int = pexConfig.DictField("", keytype=str, itemtype=int, default={})
     plot_float = pexConfig.DictField("", keytype=str, itemtype=float, default={})
     plot_bool =  pexConfig.DictField("", keytype=str, itemtype=bool, default={})
   
-def makePlotConfig(plotDict):
-    mc = PlotConfig()
+def makeMixConfig(plotDict):
+    mc = MixConfig()
     for key in plotDict.keys():
-        if type(plotDict[key]) is str:
-            mc.plot_str[key] = plotDict[key]
-        elif type(plotDict[key]) is float:
-            mc.plot_float[key] = plotDict[key]
-        elif type(plotDict[key]) is int:
-            mc.plot_int[key] = plotDict[key]
-        elif type(plotDict[key]) is bool:
-            mc.plot_bool[key] = plotDict[key]
+        if type(plotDict[key]).__module__ == np.__name__:
+            value = np.asscalar(plotDict[key])
         else:
+            value = plotDict[key]
+        if type(plotDict[key]) is str:
+            mc.plot_str[key] = value
+        elif type(plotDict[key]) is float:
+            mc.plot_float[key] = value
+        elif type(plotDict[key]) is int:
+            mc.plot_int[key] = value
+        elif type(plotDict[key]) is bool:
+            mc.plot_bool[key] = value
+        else:
+            print '%s has data type %s'%(key,type(value).__name__)
             raise Exception('Unsupported kwarg data type')
     return mc
 
 class MetricConfig(pexConfig.Config):
-    name = pexConfig.Field("", dtype=str, default='')   
-    kwargs_str = pexConfig.DictField("", keytype=str, itemtype=str, default={})
-    kwargs_int = pexConfig.DictField("", keytype=str, itemtype=int, default={})
-    kwargs_float = pexConfig.DictField("", keytype=str, itemtype=float, default={})
-    kwargs_bool = pexConfig.DictField("", keytype=str, itemtype=bool, default={})
-    plot_str = pexConfig.DictField("", keytype=str, itemtype=str, default={})
-    plot_int = pexConfig.DictField("", keytype=str, itemtype=int, default={})
-    plot_float = pexConfig.DictField("", keytype=str, itemtype=float, default={})
-    plot_bool =  pexConfig.DictField("", keytype=str, itemtype=bool, default={})
+    name = pexConfig.Field("", dtype=str, default='')
+    kwargs = pexConfig.ConfigField("kwargs for metrics", dtype=MixConfig, default=None)
+    plot = pexConfig.ConfigField("kwargs for plotting parameters", dtype=MixConfig,default=None)
     params = pexConfig.ListField("", dtype=str, default=[])
     summaryStats=pexConfig.ConfigDictField("Summary Stats to run", keytype=str, 
-                                      itemtype=PlotConfig,default={})
-    histMerge = pexConfig.ConfigField("", dtype=PlotConfig, default=None)
+                                      itemtype=MixConfig,default={})
+    histMerge = pexConfig.ConfigField("", dtype=MixConfig, default=None)
 
 class ColStackConfig(pexConfig.Config):
     """If there are extra columns that need to be added, this config can be used to pass keyword paramters"""
-    name = pexConfig.Field("", dtype=str, default='')  
-    kwargs_str = pexConfig.DictField("", keytype=str, itemtype=str, default={})
-    kwargs_int = pexConfig.DictField("", keytype=str, itemtype=int, default={})
-    kwargs_float = pexConfig.DictField("", keytype=str, itemtype=float, default={})
-    kwargs_bool = pexConfig.DictField("", keytype=str, itemtype=bool, default={})
+    name = pexConfig.Field("", dtype=str, default='')
+    kwargs = pexConfig.ConfigField("kwargs for stacker", dtype=MixConfig, default=None)
     params = pexConfig.ListField("", dtype=str, default=[])
     
 
 class BinnerConfig(pexConfig.Config):
     name = pexConfig.Field("", dtype=str, default='') # Change this to a choiceField? Or do we expect users to make new bins?
 
-    kwargs_str =  pexConfig.DictField("", keytype=str, itemtype=str, default={})
-    kwargs_float =  pexConfig.DictField("", keytype=str, itemtype=float, default={})
-    kwargs_int =  pexConfig.DictField("", keytype=str, itemtype=int, default={})
-    kwargs_bool =  pexConfig.DictField("", keytype=str, itemtype=bool, default={})
-    
+    kwargs = pexConfig.ConfigField("kwargs for binner", dtype=MixConfig, default=None)
+
     params_str =  pexConfig.ListField("", dtype=str, default=[]) 
     params_float =  pexConfig.ListField("", dtype=float, default=[]) 
     params_int =  pexConfig.ListField("", dtype=int, default=[]) 
     params_bool =  pexConfig.ListField("", dtype=bool, default=[])
-    
-    setupKwargs_str = pexConfig.DictField("", keytype=str, itemtype=str, default={})
-    setupKwargs_float = pexConfig.DictField("", keytype=str, itemtype=float, default={})
-    setupKwargs_int = pexConfig.DictField("", keytype=str, itemtype=int, default={})
-    setupKwargs_bool = pexConfig.DictField("", keytype=str, itemtype=bool, default={})
+
+    setup_kwargs = pexConfig.ConfigField("setup kwargs for binner", dtype=MixConfig, default=None)
     
     setupParams_str = pexConfig.ListField("", dtype=str, default=[])
     setupParams_float = pexConfig.ListField("", dtype=float, default=[])
@@ -72,7 +63,7 @@ class BinnerConfig(pexConfig.Config):
     metricDict = pexConfig.ConfigDictField(doc="dict of index: metric config", keytype=int, itemtype=MetricConfig, default={})
     constraints = pexConfig.ListField("", dtype=str, default=[])
     stackCols = pexConfig.ConfigDictField(doc="dict of index: ColstackConfig", keytype=int, itemtype=ColStackConfig, default={}) 
-    plotConfigs = pexConfig.ConfigDictField(doc="dict of plotConfig objects keyed by metricName", keytype=str, itemtype=PlotConfig, default={})
+    plotConfigs = pexConfig.ConfigDictField(doc="dict of plotConfig objects keyed by metricName", keytype=str, itemtype=MixConfig, default={})
     metadata = pexConfig.Field("", dtype=str, default='')
     
 class MafConfig(pexConfig.Config):
@@ -115,17 +106,7 @@ def makeBinnerConfig(name, params=[], kwargs={}, setupParams=[], setupKwargs={},
         else:
             raise Exception('Unsupported parameter data type')
 
-    for key in kwargs.keys():
-        if type(kwargs[key]) is str:
-            binner.kwargs_str[key] = kwargs[key]
-        elif type(kwargs[key]) is float:
-            binner.kwargs_float[key] = kwargs[key]
-        elif type(kwargs[key]) is int:
-            binner.kwargs_float[key] = kwargs[key]
-        elif type(kwargs[key]) is bool:
-            binner.kwargs_bool[key] = kwargs[key]
-        else:
-            raise Exception('Unsupported kwarg data type')
+    binner.kwargs = makeMixConfig(kwargs)
 
     binner.setupParams_str=[]
     binner.setupParams_float=[]
@@ -143,18 +124,7 @@ def makeBinnerConfig(name, params=[], kwargs={}, setupParams=[], setupKwargs={},
             binner.setupParams_bool.append(p) 
         else:
             raise Exception('Unsupported parameter data type')
-
-    for key in setupKwargs.keys():
-        if type(setupKwargs[key]) is str:
-            binner.setupKwargs_str[key] = setupKwargs[key]
-        elif type(setupKwargs[key]) is float:
-            binner.setupKwargs_float[key] = setupKwargs[key]
-        elif type(setupKwargs[key]) is int:
-            binner.setupKwargs_float[key] = setupKwargs[key]
-        elif type(setupKwargs[key]) is bool:
-            binner.setupKwargs_bool[key] = setupKwargs[key]
-        else:
-            raise Exception('Unsupported kwarg data type')
+    binner.setupKwargs = makeMixConfig(setupKwargs)
 
     return binner
 
@@ -166,23 +136,14 @@ def readBinnerConfig(config):
     params.append(config.params_int)
     params.append(config.params_bool)
     params = filter(None,params)
-    kwargs = {}
-    for key in config.kwargs_str:  kwargs[key] = config.kwargs_str[key]
-    for key in config.kwargs_float:  kwargs[key] = config.kwargs_float[key]
-    for key in config.kwargs_int:  kwargs[key] = config.kwargs_int[key]
-    for key in config.kwargs_bool:  kwargs[key] = config.kwargs_bool[key]
+    kwargs = readMixConfig(config.kwargs)
     setupParams=[]
     setupParams.append(config.setupParams_str)
     setupParams.append(config.setupParams_float)
     setupParams.append(config.setupParams_int)
     setupParams.append(config.setupParams_bool)
     setupParams = filter(None,setupParams)
-    setupKwargs = {}
-    for key in config.setupKwargs_str:  setupKwargs[key] = config.setupKwargs_str[key]
-    for key in config.setupKwargs_float:  setupKwargs[key] = config.setupKwargs_float[key]
-    for key in config.setupKwargs_int:  setupKwargs[key] = config.setupKwargs_int[key]
-    for key in config.setupKwargs_bool:  setupKwargs[key] = config.setupKwargs_bool[key]
-
+    setupKwargs = readMixConfig(config.setupKwargs)
     metricDict = config.metricDict    
     constraints=config.constraints
     stackCols= config.stackCols
@@ -196,50 +157,14 @@ def makeMetricConfig(name, params=[], kwargs={}, plotDict={}, summaryStats={}, h
     mc.params=params
     mc.summaryStats = {}
     for key in summaryStats:
-        mc.summaryStats[key] = makePlotConfig(summaryStats[key])
-    mc.histMerge=makePlotConfig(histMerge)
-    # Break the kwargs by data type
-    for key in kwargs.keys():
-        if type(kwargs[key]) is str:
-            mc.kwargs_str[key] = kwargs[key]
-        elif type(kwargs[key]) is float:
-            mc.kwargs_float[key] = kwargs[key]
-        elif type(kwargs[key]) is int:
-            mc.kwargs_float[key] = kwargs[key]
-        elif type(kwargs[key]) is bool:
-            mc.kwargs_bool[key] = kwargs[key]
-        else:
-            raise Exception('Unsupported kwarg data type')
-    for key in plotDict.keys():
-        if type(plotDict[key]) is str:
-            mc.plot_str[key] = plotDict[key]
-        elif type(plotDict[key]) is float:
-            mc.plot_float[key] = plotDict[key]
-        elif type(plotDict[key]) is int:
-            mc.plot_int[key] = plotDict[key]
-        elif type(plotDict[key]) is bool:
-            mc.plot_bool[key] = plotDict[key]
-        else:
-            raise Exception('Unsupported kwarg data type')
+        mc.summaryStats[key] = makeMixConfig(summaryStats[key])
+    mc.histMerge=makeMixConfig(histMerge)
+    mc.kwargs = makeMixConfig(kwargs)
+    mc.plot = makeMixConfig(plotDict)
     return mc
 
 
-
-
-#def makeHist2MergeConfig(inDict):
-#    outDict={}
-#    for key in inDict:
-#        outDict[key] = makePlotConfig(inDict[key])
-#    return outDict
-
-#def readHist2MergeConfig(config):
-#    outDict={}
-#    for key in config.hist2merge:
-#        outDict[key] = readPlotConfig(config.hist2merge[key])
-#    return outDict
-        
-
-def readPlotConfig(config):
+def readMixConfig(config):
     plotDict={}
     for key in config.plot_str:  plotDict[key] = config.plot_str[key]
     for key in config.plot_int:  plotDict[key] = config.plot_int[key]
@@ -250,23 +175,15 @@ def readPlotConfig(config):
     
 def readMetricConfig(config):
     name, params,kwargs = config2dict(config)
-    plotDict={}
     summaryStats = config.summaryStats
-    histMerge = readPlotConfig(config.histMerge)
-    for key in config.plot_str:  plotDict[key] = config.plot_str[key]
-    for key in config.plot_int:  plotDict[key] = config.plot_int[key]
-    for key in config.plot_float:  plotDict[key] = config.plot_float[key]
-    for key in config.plot_bool:  plotDict[key] = config.plot_bool[key]
+    histMerge = readMixConfig(config.histMerge)
+    plotDict = readMixConfig(config.plot)
     return name,params,kwargs,plotDict,summaryStats, histMerge
    
 
 
 def config2dict(config):
-    kwargs={}
-    for key in config.kwargs_str:  kwargs[key] = config.kwargs_str[key]
-    for key in config.kwargs_int:  kwargs[key] = config.kwargs_int[key]
-    for key in config.kwargs_float:  kwargs[key] = config.kwargs_float[key]
-    for key in config.kwargs_bool:  kwargs[key] = config.kwargs_bool[key]
+    kwargs = readMixConfig(config.kwargs)
     params=config.params
     name = config.name
     return name, params, kwargs
