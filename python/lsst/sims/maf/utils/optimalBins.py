da95af04f8b7330f93a2e3b886c5da0d4c53d6ca
Peter Yoachim <yoachim@uw.edu>
2014-06-13 16:18:34 -0700

    updated unit test
    Former-commit-id: 27f6a90f720ee751ac0e311d2c8e81af8ed5165f

--- python/lsst/sims/maf/utils/optimalBins.py
+++ python/lsst/sims/maf/utils/optimalBins.py
@@ -7,9 +7,10 @@ def optimalBins(datain):
         data = datain.compressed()
     else:
         data = datain
-    if np.size(data) == 0:
+
+    binwidth = 2.*(np.percentile(data,75) - np.percentile(data,25))/np.size(data)**(1./3.)
+    nbins = (data.max()-data.min())/binwidth
+    if np.isinf(nbins) or np.isnan(nbins):
         return 1
     else:
-        binwidth = 2.*(np.percentile(data,75) - np.percentile(data,25))/np.size(data)**(1./3.)
-        nbins = int((data.max()-data.min())/binwidth)
-        return nbins
+        return int(nbins)
