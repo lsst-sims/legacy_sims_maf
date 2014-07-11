fff74ad26e48cc7a9c9e185796f29edc47ddaed7
Lynne Jones <ljones.uw@gmail.com>
2014-07-11 14:18:15 -0700

    Added new workshop stuff (update to baseStacker, new metrics, new stackers, new examples of using these.)
    Former-commit-id: 65f18b3f3f58132fa322aa7c35d6f0001cdee963

--- examples/workshop/useNewMetrics.py
+++ examples/workshop/useNewMetrics.py
@@ -1,12 +1,21 @@
 from lsst.sims.maf.driver.mafConfig import configureMetric, configureSlicer, makeDict
 
 root.outputDir = 'Out'
-root.dbAddress = {'dbAddress':'sqlite:///../../tests/opsimblitz1_1131_sqlite.db'}
-root.opsimName = 'opsimblitz1_1131'
+root.dbAddress = {'dbAddress':'sqlite:///../tier1/opsimblitz2_1060_sqlite.db'}
+root.opsimName = 'opsimblitz2_1060'
 
 root.modules = ['exampleNewMetrics']
+
+metric = configureMetric('exampleNewMetrics.NightsWithNFiltersMetric')
+slicer = configureSlicer('HealpixSlicer', metricDict=makeDict(metric), constraints=[''])
 
 metric = configureMetric('exampleNewMetrics.SimplePercentileMetric', params=['airmass'])
 slicer = configureSlicer('UniSlicer', metricDict=makeDict(metric), constraints=['filter="r"'])
 
+metric = configureMetric('exampleNewMetrics.PercentileMetric', params=['airmass'],
+                         kwargs={'percentile':75})
+slicer = configureSlicer('UniSlicer', metricDict=makeDict(metric), constraints=['filter="r"'])
+
 root.slicers = makeDict(slicer)
+
+
