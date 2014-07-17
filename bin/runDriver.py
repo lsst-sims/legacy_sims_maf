be8386b1f614fdbc152ee8b9d16f1205bf5efe03
Peter Yoachim <yoachim@uw.edu>
2014-07-17 10:17:14 -0700

    added --version to runDriver
    Former-commit-id: 4a958badd30257431323e0cd9040584e320d2fbc

--- bin/runDriver.py
+++ bin/runDriver.py
@@ -10,21 +10,30 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Python script to interpret MAF "one-off" configuration files '
                                      'and feed them to the driver.')
     parser.add_argument("configFile", type=str, help="Name of the configuration file.")
+    parser.add_argument("--version", help="Print the current version of MAF", action="store_true")
 
     args = parser.parse_args()
 
-    # Set up configuration parameters.
-    config = MafConfig()
-    print 'Reading config data from %s' %(args.configFile)
-    config.load(args.configFile)
-    print 'Finished loading config file: %s' %(args.configFile)
+    if args.version:
+        import lsst.sims.maf.utils as utils
+        date, version = utils.getDateVersion()
+        print 'version = '+version['__version__']
+        print 'fingerprint = '+version['__fingerprint__']
+        print 'repo_version = '+version['__repo_version__']
+    else:
 
-    # Run MAF driver.
-    try:
-        drive = driver.MafDriver(config)
-    except OSError:
-        print '** %s is not a one-off driver configuration file.' %(args.configFile)
-        print '** Flexible configuration files must be run using runFlexibleDriver.py.'
-        print '** Try:  runFlexibleDriver.py %s --runName [runName] (etc)' %(args.configFile)
-        exit()
-    drive.run()
+        # Set up configuration parameters.
+        config = MafConfig()
+        print 'Reading config data from %s' %(args.configFile)
+        config.load(args.configFile)
+        print 'Finished loading config file: %s' %(args.configFile)
+
+        # Run MAF driver.
+        try:
+            drive = driver.MafDriver(config)
+        except OSError:
+            print '** %s is not a one-off driver configuration file.' %(args.configFile)
+            print '** Flexible configuration files must be run using runFlexibleDriver.py.'
+            print '** Try:  runFlexibleDriver.py %s --runName [runName] (etc)' %(args.configFile)
+            exit()
+        drive.run()
--- bin/runFlexibleDriver.py
+++ bin/runFlexibleDriver.py
@@ -18,35 +18,44 @@ if __name__=="__main__":
     parser.add_argument("--outputDir", type=str, default='./Out', help='Output directory for MAF outputs.')
     parser.add_argument("--slicerName", type=str, default='HealpixSlicer', help='SlicerName,'
                         'for flexible configuration driver scripts that take this argument.')
-
+    parser.add_argument("--version", help="Print the current version of MAF", action="store_true")
+    
     args = parser.parse_args()
-
-    # Set up configuration parameters.
-    config = MafConfig()
-    # Pull out the path and filename of the config file.
-    path, configname = os.path.split(args.configFile)
-    # And strip off an extension (.py, for example)
-    configname = os.path.splitext(configname)[0]
-    # Add the path to the configFile to the sys.path
-    if len(path) > 0:
-        sys.path.insert(0, path)
+    
+    if args.version:
+        import lsst.sims.maf.utils as utils
+        date, version = utils.getDateVersion()
+        print 'version = '+version['__version__']
+        print 'fingerprint = '+version['__fingerprint__']
+        print 'repo_version = '+version['__repo_version__']
     else:
-        sys.path.insert(0, os.getcwd())
+    
+        # Set up configuration parameters.
+        config = MafConfig()
+        # Pull out the path and filename of the config file.
+        path, configname = os.path.split(args.configFile)
+        # And strip off an extension (.py, for example)
+        configname = os.path.splitext(configname)[0]
+        # Add the path to the configFile to the sys.path
+        if len(path) > 0:
+            sys.path.insert(0, path)
+        else:
+            sys.path.insert(0, os.getcwd())
 
-    # Import the module's config method.
-    try:
-        conf = importlib.import_module(configname)
-    except NameError:
-        print '** %s is not a flexible driver configuration file.' %(configname)
-        print '** One-off configuration files must be run using runDriver.py.'
-        print '** Try:  runDriver.py %s' %(args.configFile)
-        exit()
+        # Import the module's config method.
+        try:
+            conf = importlib.import_module(configname)
+        except NameError:
+            print '** %s is not a flexible driver configuration file.' %(configname)
+            print '** One-off configuration files must be run using runDriver.py.'
+            print '** Try:  runDriver.py %s' %(args.configFile)
+            exit()
 
-    print 'Finished loading config from %s.mConfig' %(configname)        
-    # Run driver configuration script.
-    config = conf.mConfig(config, runName=args.runName, dbDir=args.dbDir, outputDir=args.outputDir,
-                            slicerName=args.slicerName)
-        
-    # Run MAF driver.
-    drive = driver.MafDriver(config)
-    drive.run()
+        print 'Finished loading config from %s.mConfig' %(configname)        
+        # Run driver configuration script.
+        config = conf.mConfig(config, runName=args.runName, dbDir=args.dbDir, outputDir=args.outputDir,
+                                slicerName=args.slicerName)
+
+        # Run MAF driver.
+        drive = driver.MafDriver(config)
+        drive.run()
