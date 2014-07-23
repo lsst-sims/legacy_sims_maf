dc6567423b75fb382173275d987846cb03154c9a
Peter Yoachim <yoachim@uw.edu>
2014-07-23 15:54:38 -0700

    started making large summary stats table--not working yet
    Former-commit-id: 527c374b7ffb44465780e5e79522cc319755341f

--- python/lsst/sims/maf/viz/vizUtils.py
+++ python/lsst/sims/maf/viz/vizUtils.py
@@ -1,5 +1,6 @@
 import lsst.sims.maf.db as db
 import numpy as np
+import numpy.lib.recfunctions as rfn
 
 def loadResults(sourceDir):
     """Load up the three tables from resultsDb_sqlite.db """
@@ -36,35 +37,55 @@ def blockAll(metrics, plots, stats):
 
 def blockSS(metrics, plots, stats):
     """Group up results to be layed out in SSTAR-like way """
+    # Set up lists that will be looped over by template
     blocks =[]
-
     completenessBlocks = []
     allStats = []
 
-    metrics.sort(order=['metricName', 'slicerName', 'sqlConstraint'])
-
+    # Stack on an extra column for the column name so we can sort on it
+    # and get "Median Airmass" next to "Rms Airmass", etc.
+     
+    metrics = rfn.merge_arrays([metrics, np.empty(metrics.size,
+                                                  dtype=[('colName','|S256')])], flatten=True, usemask=False)
+    for i in np.arange(metrics.size):
+        name = metrics['metricName'][i].split(' ')
+        if len(name) > 1:
+            metrics['colName'][i] = name[1]
+        else:
+            metrics['colName'][i] = metrics['metricName'][i]
+    
+    metrics.sort(order=['colName', 'slicerName', 'sqlConstraint'])
 
     for metric in metrics:
         mId = metric['metricId']
         relevant_plots = plots[np.where(plots['metricId'] == mId)[0]]
         for i in np.arange(relevant_plots.size):
             relevant_plots['plotFile'][i] = relevant_plots['plotFile'][i].replace('.pdf', '.png')
-
         relevant_stats = stats[np.where(stats['metricId'] == mId)[0] ]
+        
         relevant_metrics = metrics[np.where(metrics['metricId'] == mId)[0] ]
         stat_list = [(i, '%.4g'%j) for i,j in  zip(relevant_stats['summaryName'],
                                                    relevant_stats['summaryValue']) ]
+        statsDict={}
+        for rel_stat in relevant_stats:
+            statsDict[rel_stat['summaryName']] = '%.4g'%rel_stat['summaryValue']
+        allStats.append({'NameInfo':relevant_metrics['metricName'][0]+', '+
+                         relevant_metrics['slicerName'][0]
+                         + ', ' +  relevant_metrics['sqlConstraint'][0], 'stats':statsDict} )
         block = {'NameInfo': relevant_metrics['metricName'][0]+', '+
                        relevant_metrics['slicerName'][0]
                        + ', ' +  relevant_metrics['sqlConstraint'][0],
                        'plots':relevant_plots['plotFile'].tolist(),
                        'stats':stat_list}
+        # If it's a completeness metric, pull it out
         if metric['metricName'][0:12] == 'Completeness':
             completenessBlocks.append(block)
-            
         else:
             blocks.append(block)
-    
+
+
+            
+            
     return {'blocks':blocks, 'completenessBlocks':completenessBlocks, 'allStats':allStats}
 
     
