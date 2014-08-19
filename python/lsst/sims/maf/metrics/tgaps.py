24bc4ed4eb4a9b0ac12c3ca3391edce05b9364c7
Peter Yoachim <yoachim@uw.edu>
2014-08-19 14:16:45 -0700

    adding documentation
    Former-commit-id: 8b4a2b95495fe3cf62b668ec3f0820feab9db677

--- python/lsst/sims/maf/metrics/tgaps.py
+++ python/lsst/sims/maf/metrics/tgaps.py
@@ -4,12 +4,25 @@ from .baseMetric import BaseMetric
 class Tgaps(BaseMetric):
     """Histogram up all the time gaps """
 
-    def __init__(self, timesCol='expMJD', binMin=.5,
-                 binMax=60.5, binsize=0.5, plotDict=None, **kwargs):
-        """  """
+    def __init__(self, timesCol='expMJD', allGaps=False, binMin=.5,
+                 binMax=60.5, binsize=0.5, plotDict=None, units='days',
+                 **kwargs):
+        """
+        Metric to measure the gaps between observations.  By default, only gaps
+        between neighboring visits are computed.  If allGaps is set to true, all gaps are
+        computed (i.e., if there are observations at 10, 20, 30 and 40 the default will
+        return [10,10,10] while allGaps returns [10,10,10,20,20,30])
+
+        The gaps are binned into a histogram with properties set by the binMin, binMax, and binSize paramters.
+
+        timesCol = column name for the exposure times.  Assumed to be in days.
+        allGaps = should all observation gaps be computed (True), or
+                  only gaps between consecutive observations (False, default)
+        """
         if plotDict is None:
             plotDict = {}
-        
+        # Force the plotDict to use the same bins as the metric
+        # Need to do this so the slicer uses the same bins
         plotDict['binMin'] = binMin
         plotDict['binMax'] = binMax
         plotDict['binsize'] = binsize
@@ -17,17 +30,21 @@ class Tgaps(BaseMetric):
         self.timesCol = timesCol
         super(Tgaps, self).__init__(col=[self.timesCol],
                                     metricDtype=object, plotDict=plotDict,
-                                    **kwargs)
+                                    units=units, **kwargs)
         self.bins=np.arange(binMin, binMax+binsize,binsize)
+        self.allGaps = allGaps
 
     def run(self, dataSlice, slicePoint=None):
         if dataSlice.size < 2:
             return self.badval
         times = np.sort(dataSlice[self.timesCol])
-        dts = ( times - np.roll(times, 1))[1:]
-        # Convert to seconds
-        #dts = dts*24.*3600. 
-        # do we want an option that unrolls and does all possible lags?
+        if self.allGaps:
+            allDiffs = []
+            for i in np.arange(1,times.size,1):
+                allDiffs.append( (times-np.roll(times,i))[i:] )
+            dts = np.concatenate(allDiffs)
+        else:
+            dts = np.diff(times)
         result, bins = np.histogram(dts, self.bins)
         return result
         
--- python/lsst/sims/maf/slicers/healpixHistSlicer.py
+++ python/lsst/sims/maf/slicers/healpixHistSlicer.py
@@ -14,21 +14,39 @@ class HealpixHistSlicer(HealpixSlicer):
         self.plotFuncs = {'plotHistogram':self.plotHistogram}
         self.plotObject = True
 
-    def plotHistogram(self, metricValue, fignum=None, binMin=0.5,
+    def plotHistogram(self, metricValue, binMin=0.5,
                       binMax=60.5, binsize=0.5,linestyle='-',
-                      title=None,
+                      title=None, xlabel=None, units=None, ylabel=None,
+                      fignum=None, label=None, addLegend=False, legendloc='upper left',
+                      cumulative=False, xMin=None, xMax=None, yMin=None, yMax=None,
+                      logScale='auto', flipXaxis=False,
+                      yaxisformat='%.3f', color='b',
                       **kwargs):
         """ Take results of a metric that histograms things up"""
         fig = plt.figure(fignum)
+        if not xlabel:
+            xlabel = units
         
-        
+        # Need to think of best way to allow various ways to collapse.  Leave as "sum" for now,
+        # but will probably want to have mean and median as options eventually.
         finalHist = np.sum(metricValue.compressed(), axis=0)
         bins = np.arange(binMin, binMax+binsize,binsize)
         x = np.ravel(zip(bins[:-1], bins[:-1]+binsize))
         y = np.ravel(zip(finalHist, finalHist))
-        plt.plot(x,y, linestyle=linestyle)
-        plt.title(title)
-        plt.xlabel('Observation Gap Time (days)')
-        plt.ylabel('Count')
+        plt.plot(x,y, linestyle=linestyle, label=label, color=color)
+
+        if xlabel is not None:
+            plt.xlabel(xlabel)
+        if ylabel is not None:
+            plt.ylabel(ylabel)
+        if flipXaxis:
+            # Might be useful for magnitude scales.
+            x0, x1 = plt.xlim()
+            plt.xlim(x1, x0)
+        if title is not None:
+            plt.title(title)
+        if addLegend:
+            plt.legend(fancybox=True, prop={'size':'smaller'}, loc=legendloc)
+            
         return fig.number
         
