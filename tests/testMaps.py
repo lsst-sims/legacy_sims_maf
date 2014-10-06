7fb283a50ae2340805f1674fae79270f6f3ca91e
Peter Yoachim <yoachim@uw.edu>
2014-10-06 08:00:21 -0700

    make test skip if it can't find dustmap files
    Former-commit-id: c550b5a8fe8c251de2ce5990455f8a2618118436

--- tests/testMaps.py
+++ tests/testMaps.py
@@ -1,6 +1,7 @@
 import numpy as np
 import unittest
 import warnings
+import os
 
 import lsst.sims.maf.slicers as slicers
 import lsst.sims.maf.maps as maps
@@ -26,37 +27,43 @@ def makeDataValues(size=100, min=0., max=1., random=True):
 class TestMaps(unittest.TestCase):
 
     def testDustMap(self):
-        data = makeDataValues()
-        
-        dustmap = maps.DustMap()
-        
-        slicer1 = slicers.HealpixSlicer()
-        slicer1.setupSlicer(data)
-        result1 = dustmap.run(slicer1.slicePoints)
-        assert('ebv' in result1.keys())
-        
-        names=['fieldID', 'fieldRA','fieldDec']
-        types = [int, float, float]
-        fieldData = np.zeros(100, dtype=zip(names,types))
-        fieldData['fieldID'] = np.arange(100)
-        fieldData['fieldRA'] = np.random.rand(100)
-        fieldData['fieldDec'] = np.random.rand(100)
-        
-        slicer2 = slicers.OpsimFieldSlicer()
-        slicer2.setupSlicer(data, fieldData)
-        result2 = dustmap.run(slicer2.slicePoints)
-        assert('ebv' in result2.keys())
 
-        # Check interpolation works
-        dustmap = maps.DustMap(interp=True)
-        result3 = dustmap.run(slicer2.slicePoints)
+        mapPath = os.environ['SIMS_DUSTMAPS_DIR']
 
-        # Check warning gets raised
-        dustmap = maps.DustMap(nside=4)
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            result4 = dustmap.run(slicer1.slicePoints)
-            self.assertTrue("nside" in str(w[-1].message))
+        if os.path.isfile(os.path.join(mapPath, 'DustMaps/dust_nside_128.npz')):
+        
+            data = makeDataValues()
+            dustmap = maps.DustMap()
+
+            slicer1 = slicers.HealpixSlicer()
+            slicer1.setupSlicer(data)
+            result1 = dustmap.run(slicer1.slicePoints)
+            assert('ebv' in result1.keys())
+
+            names=['fieldID', 'fieldRA','fieldDec']
+            types = [int, float, float]
+            fieldData = np.zeros(100, dtype=zip(names,types))
+            fieldData['fieldID'] = np.arange(100)
+            fieldData['fieldRA'] = np.random.rand(100)
+            fieldData['fieldDec'] = np.random.rand(100)
+
+            slicer2 = slicers.OpsimFieldSlicer()
+            slicer2.setupSlicer(data, fieldData)
+            result2 = dustmap.run(slicer2.slicePoints)
+            assert('ebv' in result2.keys())
+
+            # Check interpolation works
+            dustmap = maps.DustMap(interp=True)
+            result3 = dustmap.run(slicer2.slicePoints)
+
+            # Check warning gets raised
+            dustmap = maps.DustMap(nside=4)
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                result4 = dustmap.run(slicer1.slicePoints)
+                self.assertTrue("nside" in str(w[-1].message))
+        else:
+            print 'Did not find dustmaps, not running testMaps.py'
         
 
 if __name__ == '__main__':
