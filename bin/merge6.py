f394032e64060ba313e0924a7376e5d53eef3a2f
Peter Yoachim <yoachim@uw.edu>
2014-10-29 14:09:26 -0700

    clean up, support png
    Former-commit-id: 62ede7f9eca16308b3f45d34e1663a46cad22f9d

--- bin/merge6.py
+++ bin/merge6.py
@@ -8,6 +8,11 @@ if __name__=="__main__":
     """
     Merge the u,g,r,i,z,y plots into a 3x2 grid.
     Requires pdfjam: http://www2.warwick.ac.uk/fac/sci/statistics/staff/academic-research/firth/software/pdfjam
+    Can be used with .pdf or .png files, but the output will always be a pdf
+
+    examples:
+    merge6.py ops1_1122_Median_fiveSigmaDepth_i_band_WFD_HEAL_SkyMap.pdf
+    merge6.py thumb.ops1_1122_Median_fiveSigmaDepth_u_band_all_props_HEAL_SkyMap.png
     """
 
     parser = argparse.ArgumentParser(description="Merge 6 plots into a single pdf")
@@ -21,13 +26,16 @@ if __name__=="__main__":
     for f in filters:
         if '_'+f+'_' in fileBase:
             fileBase = fileBase.split('_'+f+'_')
-            fileBase[0] = fileBase[0]
 
     fileList = [ fileBase[0]+'_'+f+'_'+fileBase[1] for f in filters]
     if args.outfile is None:
         outfile = fileBase[0]+'_6_'+fileBase[1]
     else:
         outfile = args.outfile
+
+    # can only output pdf files
+    if outfile[-3:] == 'png':
+         outfile = outfile[:-3]+'pdf'
 
     callList = ["pdfjoin", "--nup 3x2 ", "--outfile "+outfile]+fileList
     command=''
