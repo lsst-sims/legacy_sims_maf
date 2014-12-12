39d93896b8eee770ca21367f5b63756595d3c451
Lynne Jones <ljones.uw@gmail.com>
2014-12-12 15:49:51 -0800

    Updated unit tests.
    Former-commit-id: 926f5ca5be2dc16a90d58ef35f62b9eb735e8ea1

--- tests/testOpsimDb.py
+++ tests/testOpsimDb.py
@@ -17,7 +17,7 @@ class TestOpsimDb(unittest.TestCase):
         del self.oo
         self.dbAddress = None
         self.oo = None
-        
+
     def testOpsimDbSetup(self):
         """Test opsim specific database class setup/instantiation."""
         # Test tables were connected to.
@@ -35,34 +35,34 @@ class TestOpsimDb(unittest.TestCase):
 
     def testOpsimDbPropID(self):
         """Test queries for prop ID"""
-        propids, wfd, dd, propID2Name = self.oo.fetchPropIDs()
-        self.assertTrue(len(propids) > 0)
-        self.assertTrue(len(wfd) > 0)
-        self.assertTrue(len(dd) > 0)
-        for w in wfd:
+        propids, propTags = self.oo.fetchPropInfo()
+        self.assertTrue(len(propids.keys()) > 0)
+        self.assertTrue(len(propTags['WFD']) > 0)
+        self.assertTrue(len(propTags['DD']) > 0)
+        for w in propTags['WFD']:
             self.assertTrue(w in propids)
-        for d in dd:
+        for d in propTags['DD']:
             self.assertTrue(d in propids)
-        
+
     def testOpsimDbFields(self):
         """Test queries for field data."""
         # Fetch field data for all fields.
         dataAll = self.oo.fetchFieldsFromFieldTable()
         self.assertEqual(dataAll.dtype.names, ('fieldID', 'fieldRA', 'fieldDec'))
         # Fetch field data for all fields requested by a particular propid.
-        propids, wfd, dd, propID2Name = self.oo.fetchPropIDs()
-        propid = propids[0]
+        propids, proptags = self.oo.fetchPropInfo()
+        propid = propids.keys()[0]
         dataProp1 = self.oo.fetchFieldsFromFieldTable(propID=propid)
         # Fetch field data for all fields requested by all proposals.
-        dataPropAll = self.oo.fetchFieldsFromFieldTable(propID=propids)
+        dataPropAll = self.oo.fetchFieldsFromFieldTable(propID=propids.keys())
         self.assertTrue(dataProp1.size < dataPropAll.size)
         # And check that did not return multiple copies of the same field.
         self.assertEqual(len(dataPropAll['fieldID']), len(np.unique(dataPropAll['fieldID'])))
-        
+
     def testOpsimDbRunLength(self):
         """Test query for length of opsim run."""
         nrun = self.oo.fetchRunLength()
-        self.assertEqual(nrun, 0.0794)    
+        self.assertEqual(nrun, 0.0794)
 
     def testOpsimDbSimName(self):
         """Test query for opsim name."""
@@ -82,15 +82,15 @@ class TestOpsimDb(unittest.TestCase):
         self.assertTrue(isinstance(configsummary, dict))
         self.assertTrue(isinstance(configdetails, dict))
         self.assertEqual(set(configsummary.keys()), set(['Version', 'RunInfo', 'Proposals', 'keyorder']))
-        propids, wfd, dd = self.oo.fetchPropIDs()
+        propids, proptags = self.oo.fetchPropInfo()
         propidsSummary = []
         for propname in configsummary['Proposals']:
-            if propname != 'keyorder':            
+            if propname != 'keyorder':
                 propidsSummary.append(configsummary['Proposals'][propname]['PropID'])
         self.assertEqual(set(propidsSummary), set(propids))
         out.printDict(configsummary, 'Summary')
         out.printDict(configdetails, 'Details')
     '''
-       
+
 if __name__ == "__main__":
     unittest.main()
