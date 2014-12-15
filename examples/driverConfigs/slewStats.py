02c91c4fa3a928f5ed52dbc2668ad57d11a3a78e
Peter Yoachim <yoachim@uw.edu>
2014-12-15 11:06:53 -0800

    clean up example
    Former-commit-id: be65ae8249bc8f80315901fb882ad5620106b285

--- examples/driverConfigs/slewStats.py
+++ examples/driverConfigs/slewStats.py
@@ -35,107 +35,12 @@ def mConfig(config, runName, dbDir='.', outputDir='Out', slicerName='HealpixSlic
 
     config.verbose = True
 
-    #### Set up parameters for configuring plotting dictionaries and identifying WFD proposals.
-
-    # Connect to the database to fetch some values we're using to help configure the driver.
     opsimdb = utils.connectOpsimDb(config.dbAddress)
-
-    # Fetch the proposal ID values from the database
-    propids, WFDpropid, DDpropid, propID2Name = opsimdb.fetchPropIDs()
-
-    # Construct a WFD SQL where clause so multiple propIDs can query by WFD:
-    wfdWhere = ''
-    if len(WFDpropid) == 1:
-        wfdWhere = "propID = %d" %(WFDpropid[0])
-    else:
-        for i,propid in enumerate(WFDpropid):
-            if i == 0:
-                wfdWhere = wfdWhere+'('+'propID = %d ' %(propid)
-            else:
-                wfdWhere = wfdWhere+'or propID = %d ' %(propid)
-        wfdWhere = wfdWhere+')'
-
-
-    # Fetch the total number of visits (to create fraction for number of visits per proposal)
     totalNVisits = opsimdb.fetchNVisits()
     totalSlewN = opsimdb.fetchTotalSlewN()
 
-    # Filter list, and map of colors (for plots) to filters.
-    filters = ['u','g','r','i','z','y']
-    colors={'u':'m','g':'b','r':'g','i':'y','z':'r','y':'k'}
-    filtorder = {'u':1,'g':2,'r':3,'i':4,'z':5,'y':6}
-
-    # Set up benchmark values for Stretch and Design, scaled to length of opsim run.
-    runLength = opsimdb.fetchRunLength()
-    design, stretch = utils.scaleStretchDesign(runLength)
-
-    # Set zeropoints and normalization values for plots (and range for nvisits plots).
-    if benchmark == 'stretch':
-        sky_zpoints = stretch['skybrightness']
-        seeing_norm = stretch['seeing']
-        mag_zpoints = stretch['coaddedDepth']
-        nvisitBench = stretch['nvisits']
-    else:
-        sky_zpoints = design['skybrightness']
-        seeing_norm = design['seeing']
-        mag_zpoints = design['coaddedDepth']
-        nvisitBench = design['nvisits']
-    # make sure nvisitBench not zero
-    for key in nvisitBench.keys():
-        if nvisitBench[key] == 0:
-            print 'Changing nvisit benchmark value to not be zero.'
-            nvisitBench[key] = 1
-
-    # Set range of values for visits plots.
-    nVisits_plotRange = {'all':
-                         {'u':[25, 75], 'g':[50,100], 'r':[150, 200], 'i':[150, 200],
-                          'z':[100, 250], 'y':[100,250]},
-                         'DD':
-                         {'u':[6000, 10000], 'g':[2500, 5000], 'r':[5000, 8000], 'i':[5000, 8000],
-                          'z':[7000, 10000], 'y':[5000, 8000]}}
-
-    # Set slicer name and kwargs, so that we can vary these from the command line.
-    slicerNames = ['HealpixSlicer', 'HealpixSlicerDither', 'OpsimFieldSlicer']
-    if slicerName == 'HealpixSlicer':
-        slicerName = 'HealpixSlicer'
-        nside = 128
-        slicerkwargs = {'nside':nside}
-        slicermetadata = ''
-    elif slicerName == 'HealpixSlicerDither':
-        slicerName = 'HealpixSlicer'
-        nside = 128
-        slicerkwargs = {'nside':nside, 'spatialkey1':'ditheredRA', 'spatialkey2':'ditheredDec'}
-        slicermetadata = ' dithered'
-    elif slicerName == 'OpsimFieldSlicer':
-        slicerName = 'OpsimFieldSlicer'
-        slicerkwargs = {}
-        slicermetadata = ''
-    else:
-        raise ValueError('Do not understand slicerName %s: looking for one of %s' %(slicerName, slicerNames))
-    print 'Using slicer %s for generic metrics over the sky.' %(slicerName)
-
-    ####
-
-    # Configure some standard summary statistics dictionaries to apply to appropriate metrics.
-
-    # Note there's a complication here, you can't configure multiple versions of a summary metric since that makes a
-    # dict with repeated keys.  One kinda workaround is to add blank space (or even more words) to one of
-    # the keys that gets stripped out when the object is instatiated.
-    standardStats={'MeanMetric':{}, 'RmsMetric':{}, 'MedianMetric':{}, 'CountMetric':{},
-                   'NoutliersNsigma 1':{'metricName':'p3Sigma', 'nSigma':3.},
-                   'NoutliersNsigma 2':{'metricName':'m3Sigma', 'nSigma':-3.}}
-    percentileStats={'PercentileMetric 1':{'metricName':'25th%ile', 'percentile':25},
-                     'PercentileMetric 2':{'metricName':'50th%ile', 'percentile':50},
-                     'PercentileMetric 3':{'metricName':'75th%ile', 'percentile':75}}
-    allStats = standardStats.copy()
-    allStats.update(percentileStats)
-    ####
-    # Start specifying metrics and slicers for MAF to run.
-
     slicerList=[]
     histNum = 0
-
-
 
     # Calculate the mean and median slewtime.
     metricList = []
