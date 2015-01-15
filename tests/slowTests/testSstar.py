dc08026247aa9f43463af88a3efa1ddfc01852b9
Peter Yoachim <yoachim@uw.edu>
2015-01-15 11:18:25 -0800

    added sstar unit test, but it ran really slow so put in sub directory
    Former-commit-id: c1ce6bb44a51865fe28674082711ce594c10d4c1

---
+++ tests/slowTests/testSstar.py
@@ -0,0 +1,31 @@
+import matplotlib
+matplotlib.use("Agg")
+import importlib
+import os,sys, shutil
+import unittest
+from lsst.sims.maf.driver.mafConfig import MafConfig
+import lsst.sims.maf.driver as driver
+import glob
+
+class TestSstar(unittest.TestCase):
+
+    def testSstarConfig(self):
+        """Load up and run the SStar config with the small example db. """
+        configFile = glob.glob('../examples/driverConfigs/sstarDriver.py')
+        path, configname = os.path.split(configFile[0])
+        configname = os.path.splitext(configname)[0]
+        sys.path.insert(0, path)
+
+        conf = importlib.import_module(configname)
+        config = MafConfig()
+        config = conf.mConfig(config, runName='opsimblitz1_1133',
+                              outDir='Output', slicerName='OpsimFieldSlicer')
+        drive = driver.MafDriver(config)
+        drive.run()
+
+    def tearDown(self):
+        if os.path.isdir('Output'):
+            shutil.rmtree('Output')
+
+if __name__ == "__main__":
+    unittest.main()
