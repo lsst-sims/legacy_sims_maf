8f1d0fb46b480038bca047e6e138af03920168f2
Lynne Jones <ljones.uw@gmail.com>
2015-02-11 12:28:45 -0800

    Added scripts to make it easier to generate and join multiple nights of movies.
    Former-commit-id: d322c47ee6d1016af50ced8acc1a5aec5f9cc5ad

---
+++ examples/pythonScripts/joinOpsMovie.sh
@@ -0,0 +1,17 @@
+set opsRun = $1
+set nightStart = $2
+set nightEnd = $3
+echo "#Joining movie from " $opsRun " for nights " $nightStart " to " $nightEnd
+
+set nights = `seq $nightStart $nightEnd`
+set movieList = 'tmpMovieList'
+if (-e $movieList) then
+   rm $movieList
+   endif
+foreach night ( $nights )
+ echo "file "$opsRun"_n"$night"/movieFrame_SkyMap_30.0_30.0.mp4" >> $movieList
+ echo "file blank.mp4" >> $movieList
+ end
+
+ echo "ffmpeg -f concat -i "$movieList" -c copy "$opsRun"_n"$nightStart"_n"$nightEnd".mp4"
+
---
+++ examples/pythonScripts/mkOpsMovie.sh
@@ -0,0 +1,21 @@
+#!/bin/tcsh
+
+set opsRun = $1
+set nightStart = $2
+set nightEnd = $3
+echo "#Making movie from " $opsRun " for nights " $nightStart " to " $nightEnd
+
+if ($4) then
+   set sqlconstraint = $4" and"
+   echo " using general sqlconstraint " $sqlconstraint
+else
+   set sqlconstraint = ''
+endif
+
+
+set nights = `seq $nightStart $nightEnd`
+foreach night ( $nights )
+ set nightconstraint = $sqlconstraint" night="$night
+ echo "python opsimMovie.py "$opsRun"_sqlite.db --sqlConstraint "$nightconstraint" --ips 30 --outDir "$opsRun"_n"$night
+end
+
