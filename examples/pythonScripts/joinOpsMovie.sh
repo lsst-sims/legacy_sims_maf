90fd09ee9d87f0c6c93709d451b949f1da640ac5
Lynne Jones <ljones.uw@gmail.com>
2015-02-17 14:43:31 -0800

    Added documentation to mkOpsMovie.sh and joinOpsMovie.sh. Updated joinOpsMovie.sh to run ffmpeg.
    Former-commit-id: d88c8e3874ae6adcd4319623ad1fc4d895da0e11

--- examples/pythonScripts/joinOpsMovie.sh
+++ examples/pythonScripts/joinOpsMovie.sh
@@ -1,3 +1,16 @@
+#!/bin/tcsh
+
+## This script will generate AND RUN the command to concatenate mp4 files generated as movies
+## of each individual opsim night.
+## It uses ffmpeg: ffmpeg will concatenate the files included in the file 'tmpMovieList'
+## Note that the tmpMovieList file consists of a line pointing to the mp4 file, then another pointing to
+##  a special "blank.mp4" file, which is part of the git repo. The 'blank.mp4' file places a short
+##  blank space between each movie night. Adding more versions of 'blank.mp4' will add a longer pause.
+## Note that the tmpMovieList file could be edited and the ffmpeg command rerun to change these pauses, etc.
+
+## usage: expects to be run after mkOpsMovie.sh is used to generate movies for a series of individual nights.
+## usage: joinOpsMovie.sh [db name] [start night] [end night]
+
 set opsRun = $1
 set nightStart = $2
 set nightEnd = $3
@@ -16,4 +29,4 @@ foreach night ( $nights )
  end
 
 echo "ffmpeg -f concat -i "$movieList" -c copy "$opsRun"_n"$nightStart"_n"$nightEnd".mp4"
-
+ffmpeg -f concat -i "$movieList" -c copy "$opsRun"_n"$nightStart"_n"$nightEnd".mp4
--- examples/pythonScripts/mkOpsMovie.sh
+++ examples/pythonScripts/mkOpsMovie.sh
@@ -1,5 +1,12 @@
 #!/bin/tcsh
 
+## This script will generate but NOT run the commands to create movies for a series of nights from an opsim run.
+## The outputs for each night will be stored in opsRun_nX (including the movie from each individual night).
+
+## usage: mkOpsMovie.sh [opsim name] [night start] [night end]
+## after generating the movies for each individual night, join the movies together using joinOpsMovie.sh
+
+
 set opsRun = $1
 set nightStart = $2
 set nightEnd = $3
