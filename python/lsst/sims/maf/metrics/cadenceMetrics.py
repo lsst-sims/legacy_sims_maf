bd1ae6107680f8d54e2388fb70f16a29f02558c2
Peter Yoachim <yoachim@uw.edu>
2015-03-13 12:26:18 -0700

    started detect metric
    Former-commit-id: aa1393049a0095e40c94f8a52f7a2ced573599c5

--- python/lsst/sims/maf/metrics/cadenceMetrics.py
+++ python/lsst/sims/maf/metrics/cadenceMetrics.py
@@ -239,3 +239,55 @@ class QuickRevisitMetric(BaseMetric):
         condition = (counts >= self.nVisitsInNight)
         return len(counts[condition])
 
+class TransientDetectMetric(BaseBetric):
+    """
+    Calculate what fraction of the transients would be detected. Best paired with a spatial slicer.
+    We are assuming simple light curves with no color evolution.
+    """
+    def __init__(self, mjdCol='expMJD', m5Col='fiveSigmaDepth', filterCol='filter', transDuration=10.,
+                 peakTime=5., riseSlope=0., declineSlope=0.,
+                 uPeak=20, gPeak=20, rPeak=20, iPeak=20, zPeak=20, yPeak=20,
+                 surveyDuration=10., surveyStart=None, detectM5Plus=0., **kwargs):
+        """
+        transDuration = how long the transient lasts (days)
+        peakTime = How long it takes to reach the peak magnitude (days)
+        riseSlope = slope of the light curve before peak time (XXX -- should be negative since mags are backwards?)
+        declineSlope = slope of the light curve after peak time
+        (ugrizy)Peak = peak magnitude in each filter
+        surveyDuration = length of survey (years)
+        surveyStart = MJD for the survey start date (otherwise us the time of the first observation)
+        detectM5Plus = an observation will count as a detection if the light curve magnitude is brighter
+                       than m5+detectM5Plus
+        """
+        self.mjdCol = expMJD
+        self.m5Col = m5Col
+        self.fitlerCol = filterCol
+        super(TransientDetectMetric, self).__init__(col=[self.mjdCol, self.m5Col,self.filterCol], **kwargs)
+        self.peaks = {'u':uPeak,'g':gPeak,'r':rPeak,'i':iPeak,'z':zPeak,'y':yPeak}
+        self.transDuration = transDuration
+        self.peakTime = peakTime
+        self.riseSlope = riseSlope
+        self.declineSlope = declineSlope
+        self.surveyDuration = surveyDuration
+        self.surveyStart = surveyStart
+
+    def run(self, dataSlice, slicePoint=None):
+        """
+
+        """
+        # Total number of transients that could go off back-to-back
+        nTransMax = self.surveyDuration/self.transDuration
+        if self.surveyStart is None:
+            surveyStart = dataSlice[self.mjdCol].min()
+        time = (dataSlice[self.mjdCol] - surveyStart) % self.transDuration
+        lcMags = np.zeros(dataSlice.size, dtype=float)
+        for key in self.peaks.keys():
+            fMatch = np.where(dataSlice[self.filterCol] == key)
+            lcMags[fMatch] += self.peaks[key]
+        riseSec = np.where(time <= self.peakTime)
+        lcMags[riseSec] += self.riseSlope*time[riseSec]
+        declineSec = np.where(time > self.peakTime)
+        lcMags[declineSec] += self.declineSlope*time[riseSec]
+
+        detected = np.zeros(dataSlice.size, dtype=int)
+        detected[np.where(lcMags < data[self.m5Col])] = 1
