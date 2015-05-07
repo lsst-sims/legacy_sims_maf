fb5b72bc01751ffbb9f2b5e09a9bdf048768c5cf
Peter Yoachim <yoachim@uw.edu>
2015-05-07 14:11:29 -0700

    an amazingly simple unit test

---
+++ tests/testMetricBundle.py
@@ -0,0 +1,49 @@
+import unittest
+import lsst.sims.maf.metrics as metrics
+import lsst.sims.maf.slicers as slicers
+import lsst.sims.maf.metricBundles as metricBundles
+import lsst.sims.maf.db as db
+import glob
+import os
+import shutil
+
+class TestMetricBundle(unittest.TestCase):
+
+    def setUp(self):
+        self.outDir = 'TMB'
+
+    def testOut(self):
+        """
+        Check that the metric bundle can generate the expected output
+        """
+        slicer = slicers.HealpixSlicer(nside=8)
+        metric = metrics.MeanMetric(col='airmass')
+        sql = 'filter="r"'
+
+        metricB = metricBundles.MetricBundle(metric,slicer,sql)
+
+        dbAddress = 'sqlite:///opsimblitz1_1133_sqlite.db'
+        opsdb = db.OpsimDatabase(dbAddress)
+        resultsDb = db.ResultsDb(outDir=self.outDir)
+
+        bgroup = metricBundles.MetricBundleGroup({0:metricB},opsdb, outDir=self.outDir, resultsDb=resultsDb)
+        bgroup.runAll()
+        bgroup.plotAll()
+        bgroup.writeAll()
+
+        outThumbs = glob.glob(os.path.join(self.outDir, 'thumb*'))
+        outNpz = glob.glob(os.path.join(self.outDir, '*.npz'))
+        outPdf = glob.glob(os.path.join(self.outDir, '*.pdf'))
+
+        # By default, make 3 plots for healpix
+        assert(len(outThumbs) == 3)
+        assert(len(outPdf) == 3)
+        assert(len(outNpz) == 1)
+
+    def tearDown(self):
+        if os.path.isdir(self.outDir):
+            shutil.rmtree(self.outDir)
+
+
+if __name__ == "__main__":
+    unittest.main()
