be1a46b92bfc228e5e6e539fbf5b510819daeb1e
Peter Yoachim <yoachim@uw.edu>
2015-05-11 12:16:43 -0700

    first draft of NEO stacker

---
+++ python/lsst/sims/maf/stackers/NEODistStacker.py
@@ -0,0 +1,65 @@
+import numpy as np
+from .baseStacker import BaseStacker
+
+
+__all__ = ['NEODistStacker']
+
+class NEODistStacker(BaseStacker):
+    """
+    For each observation, find the max distance to a 144 km NEO, also stack on the x,y position of the asteroid
+    """
+
+    def __init__(self, m5Col='fiveSigmaDepth',
+                 stepsize=.001, maxDist=2.,H=22, elongCol='solarElong', filterCol='filter',**kwargs):
+
+        """
+        stepsize:  The stepsize to use when solving (in AU)
+        maxDist: How far out to try and measure (in AU)
+        """
+
+        self.units = ['AU','AU','AU']
+        self.colsAdded=['NEODist', 'NEOX','NEOY']
+
+        self.m5Col= m5Col
+        self.elongCol = elongCol
+        self.filterCol = filterCol
+
+        self.H = H
+        self.stepsize = stepsize
+
+        # Magic numbers that convert an asteroid V-band magnitude to LSST filters:
+        # V_5 = m_5 + (adjust value)
+        self.limitingAdjust = {'u':-2.1, 'g':-0.5,'r':0.2,'i':0.4,'z':0.6,'y':0.6}
+        self.deltas = np.arange(0,maxDist+stepsize,stepsize)
+        self.G = 0.15
+
+
+    # Phi approximations from:
+    # http://adsabs.harvard.edu/abs/2002AJ....124.1776J
+    def phi1(self,alpha):
+        result = np.exp( -3.33*np.tan(0.5*alpha)**0.63)
+        return result
+    def phi2(self,alpha):
+        result = np.exp( 1.87*np.tan(0.5*alpha)**1.22)
+
+
+    def run(self,simData, slicePoint=None):
+
+        v5 = np.zeros(simData.size, dtype=float) + simData[self.m5Col]
+        for filterName in self.limitingAdjust:
+            good = np.where(simData[self.filterCol] == filterName)
+            v5[good] += self.limitingAdjust[filterName]
+
+        for i,elong in enumerate(simData[self.elongCol]):
+            # Law of cosines:
+            R = np.sqrt(1.+self.deltas**2-2.*self.deltas*np.cos(elong) )
+            alphas = np.arccos( (1.-R**2-self.deltas**2)/(-2.*self.deltas*R) )
+            alpha_term = 2.5*np.log( (1.- self.G)*self.phi1(alphas)+self.G*self.phi2(alphas))
+            appmag = self.H+5.*np.log(R*self.deltas)-alpha_term
+            good = np.where(appmag < v5[i])
+            simData[NEODist][i] = np.max(self.deltas[good])
+
+        simData[NEOX] = -simData[NEODist]*np.cos(simData[self.elongCol])
+        simData[NEOY] = simData[NEODist]*np.sin(simData[self.elongCol])-1.
+
+        return simData
