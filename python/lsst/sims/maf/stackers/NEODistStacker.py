f73907973b9cb921b3c913979da653df4c210716
Peter Yoachim <yoachim@uw.edu>
2015-05-11 20:20:04 -0700

    getting closer

--- python/lsst/sims/maf/stackers/NEODistStacker.py
+++ python/lsst/sims/maf/stackers/NEODistStacker.py
@@ -10,7 +10,7 @@ class NEODistStacker(BaseStacker):
     """
 
     def __init__(self, m5Col='fiveSigmaDepth',
-                 stepsize=.001, maxDist=2.,H=22, elongCol='solarElong', filterCol='filter',**kwargs):
+                 stepsize=.001, maxDist=2.5,H=22, elongCol='solarElong', filterCol='filter',**kwargs):
 
         """
         stepsize:  The stepsize to use when solving (in AU)
@@ -26,23 +26,17 @@ class NEODistStacker(BaseStacker):
         self.filterCol = filterCol
 
         self.H = H
-        stepsize
-
         # Magic numbers that convert an asteroid V-band magnitude to LSST filters:
         # V_5 = m_5 + (adjust value)
         self.limitingAdjust = {'u':-2.1, 'g':-0.5,'r':0.2,'i':0.4,'z':0.6,'y':0.6}
         self.deltas = np.arange(stepsize,maxDist+stepsize,stepsize)
         self.G = 0.15
 
-
-    # Phi approximations from:
-    # http://adsabs.harvard.edu/abs/2002AJ....124.1776J
-    def phi1(self,alpha):
-        result = np.exp( -3.33*np.tan(0.5*alpha)**0.63)
-        return result
-    def phi2(self,alpha):
-        result = np.exp( 1.87*np.tan(0.5*alpha)**1.22)
-        return result
+        # magic numbers from  http://adsabs.harvard.edu/abs/2002AJ....124.1776J
+        self.a1 = -3.33
+        self.b1 = 0.63
+        self.a2 = 1.87
+        self.b2 = 1.22
 
 
     def run(self,simData, slicePoint=None):
@@ -53,20 +47,25 @@ class NEODistStacker(BaseStacker):
 
         v5 = np.zeros(simData.size, dtype=float) + simData[self.m5Col]
         for filterName in self.limitingAdjust:
-            good = np.where(simData[self.filterCol] == filterName)
-            v5[good] += self.limitingAdjust[filterName]
+            fmatch = np.where(simData[self.filterCol] == filterName)
+            v5[fmatch] += self.limitingAdjust[filterName]
 
         for i,elong in enumerate(elongRad):
             # Law of cosines:
+            # Heliocentric Radius of the object
             R = np.sqrt(1.+self.deltas**2-2.*self.deltas*np.cos(elong) )
+            # Angle between sun and earth as seen by NEO
             alphas = np.arccos( (1.-R**2-self.deltas**2)/(-2.*self.deltas*R) )
-            alpha_term = 2.5*np.log( (1.- self.G)*self.phi1(alphas)+self.G*self.phi2(alphas))
-            appmag = self.H+5.*np.log(R*self.deltas)-alpha_term
+            ta2 = np.tan(alphas/2.)
+            phi1 = np.exp(-self.a1*ta2**self.b1)
+            phi2 = np.exp(-self.a2*ta2**self.b2)
+
+            alpha_term = 2.5*np.log10( (1.- self.G)*phi1+self.G*phi2)
+            appmag = self.H+5.*np.log10(R*self.deltas)-alpha_term
             good = np.where(appmag < v5[i])
             simData['NEODist'][i] = np.max(self.deltas[good])
 
-        simData['NEOY'] = -simData['NEODist']*np.cos(elongRad)
-        simData['NEOX'] = simData['NEODist']*np.sin(elongRad)
-
+        simData['NEOX'] = simData['NEODist']*np.cos(elongRad-np.pi/2.)
+        simData['NEOY'] = simData['NEODist']*np.sin(elongRad-np.pi/2.)
 
         return simData
