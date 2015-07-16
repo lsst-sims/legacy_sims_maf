f9d667f6fe93da57b9d6969e5e092ed709578b28
Peter Yoachim <yoachim@uw.edu>
2015-07-16 16:26:50 -0700

    starting runMetricBundle

--- python/lsst/sims/maf/utils/bundleList2Dicts.py
+++ python/lsst/sims/maf/utils/bundleList2Dicts.py
@@ -1,27 +1,5 @@
 
-__all__ = ['bundleMatch', 'bundleList2Dicts']
-
-def bundleMatch(bundle1, bundle2):
-    """
-    Check if 2 metric bundle objects are compatible to be in the same
-    metricBundleGroup.
-    """
-    result = True
-    if bundle1.sqlconstraint != bundle2.sqlconstraint:
-        return False
-    for stacker1 in bundle1.stackerList:
-        for stacker2 in bundle2.stackerList:
-            if stacker1.__class__.__name__ == stacker2.__class__.__name__:
-                if stacker1 != stacker2:
-                    return False
-
-    # Possible bug in slicer comparison, with != and == giving same results
-    if bundle1.slicer == bundle2.slicer:
-        pass
-    else:
-        return False
-
-    return result
+__all__ = ['bundleList2Dicts']
 
 
 def list2Dict(inlist):
@@ -35,7 +13,6 @@ def bundleList2Dicts(bundleList):
     Take a list of metricBundle objects and consolidate them into
     lists with the same sqlConstriant
     """
-
     sqls = list(set([b.sqlconstraint for b in bundleList ]))
 
     resultDict = {}
