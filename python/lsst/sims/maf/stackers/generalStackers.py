504c14a851a40648b735e7aaded37abe7be289e3
Lynne Jones <lynnej@uw.edu>
2015-09-22 16:21:15 -0700

    Updated general stackers to use new _run method and added SeasonStacker

--- python/lsst/sims/maf/stackers/generalStackers.py
+++ python/lsst/sims/maf/stackers/generalStackers.py
@@ -9,6 +9,11 @@ from .baseStacker import BaseStacker
 __all__ = ['NormAirmassStacker', 'ParallaxFactorStacker', 'HourAngleStacker',
             'FilterColorStacker', 'ZenithDistStacker', 'ParallacticAngleStacker']
 
+# Original stackers by Peter Yoachim (yoachim@uw.edu)
+# Filter color stacker by Lynne Jones (lynnej@uw.edu)
+# Season stacker by Phil Marshall (dr.phil.marshall@gmail.com), modified by Humna Awan (humna.awan@rutgers.edu)
+
+
 ### Normalized airmass
 class NormAirmassStacker(BaseStacker):
     """
@@ -23,14 +28,13 @@ class NormAirmassStacker(BaseStacker):
         self.decCol = decCol
         self.telescope_lat = telescope_lat
 
-    def run(self, simData):
+    def _run(self, simData):
         """Calculate new column for normalized airmass."""
         # Run method is required to calculate column.
         # Driver runs getColInfo to know what columns are needed from db & which are calculated,
         #  then gets data from db and then calculates additional columns (via run methods here).
         min_z_possible = np.abs(simData[self.decCol] - np.radians(self.telescope_lat))
         min_airmass_possible = 1./np.cos(min_z_possible)
-        simData=self._addStackers(simData)
         simData['normairmass'] = simData[self.airmassCol] / min_airmass_possible
         return simData
 
@@ -39,17 +43,14 @@ class ZenithDistStacker(BaseStacker):
     Calculate the zenith distance for each pointing.
     """
     def __init__(self,altCol = 'altitude'):
-
         self.altCol = altCol
         self.units = ['radians']
         self.colsAdded = ['zenithDistance']
         self.colsReq = [self.altCol]
 
-    def run(self, simData):
+    def _run(self, simData):
         """Calculate new column for zenith distance."""
-
         zenithDist = np.pi-simData[self.altCol]
-        simData=self._addStackers(simData)
         simData['zenithDistance'] = zenithDist
         return simData
 
@@ -78,7 +79,7 @@ class ParallaxFactorStacker(BaseStacker):
         y = (np.cos(Deccen)*np.sin(Dec1) - np.sin(Deccen)*np.cos(Dec1)*np.cos(RA1-RAcen)) / cosc
         return x, y
 
-    def run(self, simData):
+    def _run(self, simData):
         ra_pi_amp = np.zeros(np.size(simData), dtype=[('ra_pi_amp','float')])
         dec_pi_amp = np.zeros(np.size(simData), dtype=[('dec_pi_amp','float')])
         ra_geo1 = np.zeros(np.size(simData), dtype='float')
@@ -95,7 +96,6 @@ class ParallaxFactorStacker(BaseStacker):
         x_geo, y_geo = self._gnomonic_project_toxy(ra_geo, dec_geo, simData[self.raCol], simData[self.decCol])
         ra_pi_amp[:] = np.degrees(x_geo1-x_geo)*3600.
         dec_pi_amp[:] = np.degrees(y_geo1-y_geo)*3600.
-        simData=self._addStackers(simData)
         simData['ra_pi_amp'] = ra_pi_amp
         simData['dec_pi_amp'] = dec_pi_amp
         return simData
@@ -111,7 +111,7 @@ class HourAngleStacker(BaseStacker):
         self.lstCol = lstCol
         self.RaCol = RaCol
 
-    def run(self, simData):
+    def _run(self, simData):
         """HA = LST - RA """
         if len(simData) == 0:
             return simData
@@ -125,7 +125,6 @@ class HourAngleStacker(BaseStacker):
         # Wrap the results so HA between -pi and pi
         ha = np.where(ha < -np.pi, ha+2.*np.pi, ha)
         ha = np.where(ha > np.pi, ha-2.*np.pi, ha)
-        simData=self._addStackers(simData)
         # Convert radians to hours
         simData['HA'] = ha*12/np.pi
         return simData
@@ -155,8 +154,7 @@ class ParallacticAngleStacker(BaseStacker):
         self.colsAdded = ['PA']
         self.colsReq = [self.raCol, self.decCol, self.mjdCol]
 
-    def run(self,simData):
-        simData = self._addStackers(simData)
+    def _run(self, simData):
         alt, az, pa = _altAzPaFromRaDec(simData[self.raCol], simData[self.decCol], self.lonRad,
                                        self.latRad,simData[self.mjdCol])
         simData['PA'] = pa
@@ -182,9 +180,8 @@ class FilterColorStacker(BaseStacker):
         # Values required for framework operation: this specifies the data columns required from the database.
         self.colsReq = [self.filterCol]
 
-    def run(self, simData):
-        # Add new columns to simData, ready to fill with new values.
-        simData = self._addStackers(simData)
+
+    def _run(self, simData):
         # Translate filter names into numbers.
         filtersUsed = np.unique(simData[self.filterCol])
         for f in filtersUsed:
@@ -195,3 +192,44 @@ class FilterColorStacker(BaseStacker):
             simData['gRGB'][match] = self.filter_rgb_map[f][1]
             simData['bRGB'][match] = self.filter_rgb_map[f][2]
         return simData
+
+
+class SeasonStacker(Basestacker):
+    """
+    Add an integer label to show which season a given visit is in.
+    The season only depends on the RA of the object: we compute the MJD
+    when each object is on the meridian at midnight, and subtract 6
+    months to get the start date of each season.
+    The season index range is 0-10.
+    Must wrap 0th and 10th to get a total of 10 seasons.
+    """
+    def __init__(self, expMJDCol='expMJD',RACol='fieldRA'):
+        # Names of columns we want to add.
+        self.colsAdded = ['year', 'season']
+        # Names of columns we need from database.
+        self.colsReq = [expMJDCol, RACol]
+        # List of units for our new columns.
+        self.units = ['','']
+        # And save the column names.
+        self.expMJDCol = expMJDCol
+        self.RACol = RACol
+
+    def _run(self, simData):
+        # Define year number: (note that opsim defines "years" in flat 365 days).
+        year = np.floor((simData[self.expMJDCol] - simData[self.expMJDCol][0]) / 365)
+        objRA= np.degrees(simData[self.RACol])/15.0   # in hrs
+        # objRA=0 on autumnal equinox.
+        # autumnal equinox 2014 happened on Sept 23 --> Equinox MJD
+        Equinox = 2456923.5 - 2400000.5
+        # Use 365.25 for the length of a year here, because we're dealing with real seasons.
+        daysSinceEquinox = 0.5*objRA*(365.25/12.0)  # 0.5 to go from RA to month; 365.25/12.0 for months to days
+        firstSeasonBegan = Equinox + daysSinceEquinox - 0.5*365.25   # in MJD
+        # Now we can compute the number of years since the first season
+        # began, and so assign a global integer season number:
+        globalSeason = np.floor((simData[self.expMJDCol] - firstSeasonBegan)/365.25)
+        # Subtract off season number of first observation:
+        season = globalSeason - np.min(globalSeason)
+        simData['year'] = year
+        simData['season'] = season
+        return simData
+
