174ecba93c620575af84c78027eb137036ba62e0
Lynne Jones <lynnej@uw.edu>
2015-09-30 17:05:06 -0700

    Added moving object plotters

--- python/lsst/sims/maf/plots/moPlotters.py
+++ python/lsst/sims/maf/plots/moPlotters.py
@@ -21,8 +21,12 @@ class MetricVsH(BasePlotter):
         self.plotType = 'MetricVsH'
         self.objectPlotter = False
         self.defaultPlotDict = {'title':None, 'xlabel':'H (mag)', 'ylabel':None, 'label':None,
+<<<<<<< ba7f1d2106e03356b8a84c4c82033ab9ab6c08fc
                                 'npReduce':None, 'nbins':None, 'albedo':None,
                                 'Hmark':None}
+=======
+                                'npReduce':None, 'nbins':None, 'albedo':None, 'Hmark':None}
+>>>>>>> Added moving object plotters
         self.minHrange=1.0
 
     def __call__(self, metricValue, slicer, userPlotDict, fignum=None):
