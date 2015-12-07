6cd57f8ad29fcf34d6970791545d11de65d522ea
Peter Yoachim <yoachim@uw.edu>
2015-12-07 10:47:19 -0800

    final tweek

--- python/lsst/sims/maf/maps/stellarDensityMap.py
+++ python/lsst/sims/maf/maps/stellarDensityMap.py
@@ -12,16 +12,17 @@ class StellarDensityMap(BaseMap):
     Return the cumulative stellar luminosity function for each slicepoint. Units of stars per sq degree.
     Uses a healpix map of nside=64. Uses the nearest healpix point for other ra,dec values.
     """
-    def __init__(self, keyname='starLumFunc'):
+    def __init__(self, keyname='starLumFunc', filtername='r'):
         """
 
         """
         self.keyNames = [keyname, 'starMapBins']
         self.mapDir = os.path.join(getPackageDir('sims_dustmaps'),'StarMaps')
+        self.filtername = filtername
 
 
     def _readMap(self):
-        filename = 'starDensity_nside_64.npz'
+        filename = 'starDensity_%s_nside_64.npz' % self.filtername
         starMap = np.load(os.path.join(self.mapDir,filename))
         self.starMap = starMap['starDensity'].copy()
         self.starMapBins = starMap['bins'].copy()
