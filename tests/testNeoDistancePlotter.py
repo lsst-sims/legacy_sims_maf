41505fa7b329ee23c0f2bdfec8dea5c86ced0e68
Peter Yoachim <yoachim@uw.edu>
2016-01-05 16:17:48 -0500

    NEO plotter test

---
+++ tests/testNeoDistancePlotter.py
@@ -0,0 +1,26 @@
+import matplotlib
+matplotlib.use("Agg")
+import numpy as np
+import unittest
+import lsst.sims.maf.plots as plots
+
+class TestNeoDistancePlotter(unittest.TestCase):
+    def setUp(self):
+        names = ['eclipLat','eclipLon', 'MaxGeoDist',
+                 'NEOHelioX', 'NEOHelioY', 'filter']
+        types = [float]*5
+        types.append('|S1')
+        npts = 100
+        self.metricValues = np.zeros(npts, zip(names,types))
+        self.metricValues['MaxGeoDist'] = np.random.rand(npts)*2.
+        self.metricValues['eclipLat'] = np.random.rand(npts)
+        self.metricValues['NEOHelioX'] = np.random.rand(npts)*3-1.5
+        self.metricValues['NEOHelioY'] = np.random.rand(npts)*3-1.5+1
+        self.metricValues['filter'] = 'g'
+
+    def testPlotter(self):
+        """
+        Just test that it can make a figure without throwing an error.
+        """
+        plotter = plots.NeoDistancePlotter()
+        fig = plotter(self.metricValues, None, None)
