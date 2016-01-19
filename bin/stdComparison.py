7957c9003a6012cb56e29bf68a86609c1463f140
Lynne Jones <lynnej@uw.edu>
2016-01-19 15:55:59 -0800

    final review tweaks

--- bin/stdComparison.py
+++ bin/stdComparison.py
@@ -2,31 +2,48 @@ import os
 import argparse
 from lsst.sims.maf.viz import MafRunComparison
 
+# Make the output of this file be 'nice' to read back into pandas.
 def pandaprint(stats):
     for i in range(len(stats)):
         writestring = ''
         for j in range(len(stats[i])):
-            writestring += ' %s;' % (stats[i][j])
-        print writestring.lstrip(' ').rstrip(';')
+            writestring += '%s,' % (stats[i][j])
+        print writestring.lstrip(' ').rstrip(',')
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run a simple comparison of a set of defined metrics between'
-                                     ' multiple opsim runs.')
+                                     ' multiple opsim runs.', formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--baseDir', type=str, default=None,
-                        help='Root directory containing:'
-                        ' file with list of runs and top directory containing MAF results.')
+                        help='Root directory containing the MAF results.')
     parser.add_argument('--runlist', type=str, default=None,
-                        help='File containing the names of the runs to compare '
-                        '(and optionally the directories, relative to baseDir, where they reside).')
+                        help='File containing the names of the runs to compare\n'
+                        '(and optionally the paths to each directory to use, relative to baseDir).'
+                        '\n '
+                        'Example: (using specific subdirectories under minion_1016/ and minion_1012/)'
+                        '\n '
+                        'minion_1016    minion_1016/scheduler'
+                        '\n '
+                        'minion_1016    minion_1016/science'
+                        '\n '
+                        'minion_1012    minion_1012/scheduler'
+                        '\n '
+                        'minion_1012    minion_1012/science2'
+                        '\n '
+                        'Example: (using all subdirectories under minion_1016/ and minion_1012/)'
+                        '\n '
+                        'minion_1016'
+                        '\n '
+                        'minion_1012'
+                        '\n ')
     parser.set_defaults()
     args = parser.parse_args()
 
     baseDir = args.baseDir
 
-    # Read the runs to be compared from a file called 'tier1.txt'
-    f = open(os.path.join(baseDir, args.runlist), 'r')
+    # Read the runs to be compared from 'runlist'.
+    f = open(os.path.join(args.runlist), 'r')
     runlist = []
     rundirs = []
     for line in f:
@@ -40,8 +57,8 @@ if __name__ == '__main__':
 
     writestring = 'Summary_Name; '
     for r in runlist:
-        writestring += '%s; ' % r
-    print writestring.rstrip('; ')
+        writestring += '%s,' % r
+    print writestring.rstrip(',')
 
     # Get 'overview' statistics.
 
@@ -72,11 +89,35 @@ if __name__ == '__main__':
     summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
     pandaprint(summary)
 
+    # Number of nights with observations
+    metricName = 'Nights with observations'
+    metricMetadata = 'All Visits'
+    slicerName = 'UniSlicer'
+    summaryName = '(days)'
+    summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
+    pandaprint(summary)
+
+    # Median number of visits per night
+    metricName = 'NVisits'
+    metricMetadata = 'Per night'
+    slicerName = 'OneDSlicer'
+    summaryName = 'Median'
+    summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
+    pandaprint(summary)
+
     # Median open shutter fraction
     metricName = 'OpenShutterFraction'
     metricMetadata = 'Per night'
     slicerName = None
     summaryName = 'Median'
+    summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
+    pandaprint(summary)
+
+    # Mean slew time
+    metricName = 'Mean slewTime'
+    slicerName = None
+    metricMetadata = None
+    summaryName = None
     summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
     pandaprint(summary)
 
@@ -89,30 +130,6 @@ if __name__ == '__main__':
             summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName,
                                                   summaryName=summaryName)
             pandaprint(summary)
-
-    # Median number of visits per night
-    metricName = 'NVisits'
-    metricMetadata = 'Per night'
-    slicerName = 'OneDSlicer'
-    summaryName = 'Median'
-    summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
-    pandaprint(summary)
-
-    # Number of nights with observations
-    metricName = 'Nights with observations'
-    metricMetadata = 'All Visits'
-    slicerName = 'UniSlicer'
-    summaryName = '(days)'
-    summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
-    pandaprint(summary)
-
-    # Mean slew time
-    metricName = 'Mean slewTime'
-    slicerName = None
-    metricMetadata = None
-    summaryName = None
-    summary = runCompare.findSummaryStats(metricName, metricMetadata, slicerName, summaryName=summaryName)
-    pandaprint(summary)
 
     # fO NV and Area
     metricName = 'fO'
@@ -205,7 +222,7 @@ if __name__ == '__main__':
 
     # Median r and i band seeing
     metricName = 'Median FWHMeff'
-    slicerName = None
+    slicerName = 'UniSlicer'
     summaryName = None
     metricMetadata = []
     for f in (['r', 'i']):
