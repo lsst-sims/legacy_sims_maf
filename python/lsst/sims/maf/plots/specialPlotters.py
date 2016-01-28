d4a69a81ad4b27a7d0a5be412c2813cdd143f139
Lynne Jones <lynnej@uw.edu>
2016-01-27 17:43:32 -0800

    flake8 and update comments for most plotters

--- python/lsst/sims/maf/plots/specialPlotters.py
+++ python/lsst/sims/maf/plots/specialPlotters.py
@@ -1,10 +1,6 @@
 import numpy as np
-import warnings
 import healpy as hp
-from matplotlib import colors
-from matplotlib import ticker
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
 import lsst.sims.maf.metrics as metrics
 
@@ -13,19 +9,36 @@ from .plotHandler import BasePlotter
 __all__ = ['FOPlot', 'SummaryHistogram']
 
 class FOPlot(BasePlotter):
+    """
+    Special plotter to generate and label fO plots.
+    """
     def __init__(self):
         self.plotType = 'FO'
         self.objectPlotter = False
-        self.defaultPlotDict = {'title':None, 'xlabel':'Number of visits',
-                                'ylabel':'Area (1000s of square degrees)',
-                                'scale':None, 'Asky':18000., 'Nvisits':825,
-                                'xMin':None, 'xMax':None, 'yMin':None, 'yMax':None,
-                                'linewidth':2, 'reflinewidth':2}
+        self.defaultPlotDict = {'title': None, 'xlabel': 'Number of visits',
+                                'ylabel': 'Area (1000s of square degrees)',
+                                'scale': None, 'Asky': 18000., 'Nvisits': 825,
+                                'xMin': None, 'xMax': None, 'yMin': None, 'yMax': None,
+                                'linewidth': 2, 'reflinewidth': 2}
 
     def __call__(self, metricValue, slicer, userPlotDict, fignum=None):
         """
-        Note that Asky and Nvisit need to be set for both the slicer and the summary statistic
-          for the plot and returned summary stat values to be consistent!
+        Parameters
+        ----------
+        metricValue : numpy.ma.MaskedArray
+            The metric values calculated with the 'Count' metric and a healpix slicer.
+        slicer : lsst.sims.maf.slicers.HealpixSlicer
+        userPlotDict: dict
+            Dictionary of plot parameters set by user (overrides default values).
+            Note that Asky and Nvisits values set here and in the slicer should be consistent,
+            for plot labels and summary statistic values to be consistent.
+        fignum : int
+            Matplotlib figure number to use (default = None, starts new figure).
+
+        Returns
+        -------
+        int
+           Matplotlib figure number used to create the plot.
         """
         if not hasattr(slicer, 'nside'):
             raise ValueError('FOPlot to be used with healpix or healpix derived slicers.')
@@ -35,31 +48,32 @@ class FOPlot(BasePlotter):
         plotDict.update(userPlotDict)
 
         if plotDict['scale'] is None:
-            plotDict['scale'] = (hp.nside2pixarea(slicer.nside, degrees=True)  / 1000.0)
+            plotDict['scale'] = (hp.nside2pixarea(slicer.nside, degrees=True) / 1000.0)
 
         # Expect metricValue to be something like number of visits
-        cumulativeArea = np.arange(1,metricValue.compressed().size+1)[::-1]*plotDict['scale']
-        plt.plot(np.sort(metricValue.compressed()), cumulativeArea,'k-', linewidth=plotDict['linewidth'], zorder = 0)
+        cumulativeArea = np.arange(1, metricValue.compressed().size + 1)[::-1] * plotDict['scale']
+        plt.plot(np.sort(metricValue.compressed()), cumulativeArea, 'k-',
+                 linewidth=plotDict['linewidth'], zorder=0)
         # This is breaking the rules and calculating the summary stats in two places.
         # Could just calculate summary stats and pass in labels.
         rarr = np.array(zip(metricValue.compressed()),
-                dtype=[('fO', metricValue.dtype)])
+                        dtype=[('fO', metricValue.dtype)])
         fOArea_value = metrics.fOArea(col='fO', Asky=plotDict['Asky'], norm=False,
-                                        nside=slicer.nside).run(rarr)
+                                      nside=slicer.nside).run(rarr)
         fONv_value = metrics.fONv(col='fO', Nvisit=plotDict['Nvisits'], norm=False,
-                                    nside=slicer.nside).run(rarr)
+                                  nside=slicer.nside).run(rarr)
         fOArea_value_n = metrics.fOArea(col='fO', Asky=plotDict['Asky'], norm=True,
                                         nside=slicer.nside).run(rarr)
-        fONv_value_n = metrics.fONv(col='fo',Nvisit=plotDict['Nvisits'], norm=True,
+        fONv_value_n = metrics.fONv(col='fo', Nvisit=plotDict['Nvisits'], norm=True,
                                     nside=slicer.nside).run(rarr)
 
         plt.axvline(x=plotDict['Nvisits'], linewidth=plotDict['reflinewidth'], color='b')
-        plt.axhline(y=plotDict['Asky']/1000., linewidth=plotDict['reflinewidth'],color='r')
+        plt.axhline(y=plotDict['Asky'] / 1000., linewidth=plotDict['reflinewidth'], color='r')
 
-        plt.axhline(y=fONv_value/1000., linewidth=plotDict['reflinewidth'], color='b',
-                    alpha=.5, label=r'f$_0$ Nvisits=%.3g' %fONv_value_n)
-        plt.axvline(x=fOArea_value , linewidth=plotDict['reflinewidth'], color='r',
-                    alpha=.5, label='f$_0$ Area=%.3g'%fOArea_value_n)
+        plt.axhline(y=fONv_value / 1000., linewidth=plotDict['reflinewidth'], color='b',
+                    alpha=.5, label=r'f$_0$ Nvisits=%.3g' % fONv_value_n)
+        plt.axvline(x=fOArea_value, linewidth=plotDict['reflinewidth'], color='r',
+                    alpha=.5, label='f$_0$ Area=%.3g' % fOArea_value_n)
         plt.legend(loc='lower left', fontsize='small', numpoints=1)
 
         plt.xlabel(plotDict['xlabel'])
@@ -71,27 +85,52 @@ class FOPlot(BasePlotter):
         yMin = plotDict['yMin']
         yMax = plotDict['yMax']
         if (xMin is not None) or (xMax is not None):
-            plt.xlim([xMin,xMax])
+            plt.xlim([xMin, xMax])
         if (yMin is not None) or (yMax is not None):
-            plt.ylim([yMin,yMax])
+            plt.ylim([yMin, yMax])
         return fig.number
 
 
 class SummaryHistogram(BasePlotter):
+    """
+    Special plotter to summarize metrics which return a set of values at each slicepoint,
+    such as if a histogram was calculated at each slicepoint
+    (e.g. with the lsst.sims.maf.metrics.TgapsMetric).
+    Effectively marginalizes the calculated values over the sky, and plots the a summarized
+    version (reduced to a single according to the plotDict['metricReduce'] metric).
+    """
+
     def __init__(self):
         self.plotType = 'SummaryHistogram'
         self.objectPlotter = True
-        self.defaultPlotDict = {'title':None, 'xlabel':None, 'ylabel':'Count', 'label':None,
-                                'cumulative':False, 'xMin':None, 'xMax':None, 'yMin':None, 'yMax':None,
-                                'color':'b', 'linestyle':'-', 'histStyle':True,
-                                'metricReduce':metrics.SumMetric(), 'bins':None}
+        self.defaultPlotDict = {'title': None, 'xlabel': None, 'ylabel': 'Count', 'label': None,
+                                'cumulative': False, 'xMin': None, 'xMax': None, 'yMin': None, 'yMax': None,
+                                'color': 'b', 'linestyle': '-', 'histStyle': True,
+                                'metricReduce': metrics.SumMetric(), 'bins': None}
 
     def __call__(self, metricValue, slicer, userPlotDict, fignum=None):
         """
-        This plotting method takes the set of values @ each metricValue (i.e. a histogram at each slicer point)
-        and consolidates them into a single histogram that is plotted, effectively marginalizing over the sky.
-        Note that the plotDict['bins'] here should match the bins used with the metric and that
-        plotDict['metricReduce'] will specify the metric used to combine the histograms bin-by-bin from each slicepoint.
+        Parameters
+        ----------
+        metricValue : numpy.ma.MaskedArray
+            Handles 'object' datatypes for the masked array.
+        slicer : lsst.sims.maf.slicers
+            Any MAF slicer.
+        userPlotDict: dict
+            Dictionary of plot parameters set by user (overrides default values).
+            'metricReduce' (an lsst.sims.maf.metric) indicates how to marginalize the metric values
+            calculated at each point to a single series of values over the sky.
+            'histStyle' (True/False) indicates whether to plot the results as a step histogram (True)
+            or as a series of values (False)
+            'bins' (np.ndarray) sets the x values for the resulting plot and should generally match
+            the bins used with the metric.
+        fignum : int
+            Matplotlib figure number to use (default = None, starts new figure).
+
+        Returns
+        -------
+        int
+           Matplotlib figure number used to create the plot.
         """
         fig = plt.figure(fignum)
         plotDict = {}
@@ -103,14 +142,14 @@ class SummaryHistogram(BasePlotter):
         # Get the data type
         dt = metricValue.compressed()[0].dtype
         # Change an array of arrays (dtype=object) to a 2-d array of correct dtype
-        mV = np.array(metricValue.compressed().tolist(), dtype=[('metricValue',dt)])
+        mV = np.array(metricValue.compressed().tolist(), dtype=[('metricValue', dt)])
         # Make an array to hold the combined result
         finalHist = np.zeros(mV.shape[1], dtype=float)
         metric = plotDict['metricReduce']
         metric.colname = 'metricValue'
         # Loop over each bin and use the selected metric to combine the results
         for i in np.arange(finalHist.size):
-            finalHist[i] = metric.run(mV[:,i])
+            finalHist[i] = metric.run(mV[:, i])
         bins = plotDict['bins']
         if plotDict['histStyle']:
             x = np.ravel(zip(bins[:-1], bins[1:]))
@@ -120,7 +159,8 @@ class SummaryHistogram(BasePlotter):
             x = bins[:-1]
             y = finalHist
         # Make the plot.
-        plt.plot(x,y, linestyle=plotDict['linestyle'], label=plotDict['label'], color=plotDict['color'])
+        plt.plot(x, y, linestyle=plotDict['linestyle'],
+                 label=plotDict['label'], color=plotDict['color'])
         # Add labels.
         plt.xlabel(plotDict['xlabel'])
         plt.ylabel(plotDict['ylabel'])
--- python/lsst/sims/maf/plots/twoDPlotters.py
+++ python/lsst/sims/maf/plots/twoDPlotters.py
@@ -1,13 +1,10 @@
 import numpy as np
 from matplotlib import colors
-from matplotlib import ticker
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 from .plotHandler import BasePlotter
 from .perceptual_rainbow import makePRCmap
 
-__all__=['TwoDMap', 'VisitPairsHist']
-
+__all__ = ['TwoDMap', 'VisitPairsHist']
 
 perceptual_rainbow = makePRCmap()
 
@@ -15,16 +12,31 @@ class TwoDMap(BasePlotter):
     def __init__(self):
         self.plotType = 'TwoD'
         self.objectPlotter = False
-        self.defaultPlotDict = {'title':None, 'xlabel':None, 'ylabel':None, 'label':None,
-                                'logScale':False, 'cbarFormat':None, 'cbarTitle': 'Count' ,
-                                'cmap':perceptual_rainbow,
-                                'percentileClip':None, 'colorMin':None, 'colorMax':None,
-                                'zp':None, 'normVal':None,
-                                'cbar_edge':True, 'nTicks':None, 'aspect':'auto',
-                                'xextent':None, 'origin':None}
+        self.defaultPlotDict = {'title': None, 'xlabel': None, 'ylabel': None, 'label': None,
+                                'logScale': False, 'cbarFormat': None, 'cbarTitle': 'Count',
+                                'cmap': perceptual_rainbow,
+                                'percentileClip': None, 'colorMin': None, 'colorMax': None,
+                                'zp': None, 'normVal': None,
+                                'cbar_edge': True, 'nTicks': None, 'aspect': 'auto',
+                                'xextent': None, 'origin': None}
 
     def __call__(self, metricValue, slicer, userPlotDict, fignum=None):
+        """
+        Parameters
+        ----------
+        metricValue : numpy.ma.MaskedArray
+        slicer : lsst.sims.maf.slicers.BaseSpatialSlicer
+            (any spatial slicer)
+        userPlotDict: dict
+            Dictionary of plot parameters set by user (overrides default values).
+        fignum : int
+            Matplotlib figure number to use (default = None, starts new figure).
 
+        Returns
+        -------
+        int
+           Matplotlib figure number used to create the plot.
+        """
         if 'Healpix' in slicer.slicerName:
             self.defaultPlotDict['ylabel'] = 'Healpix ID'
         elif 'Opsim' in slicer.slicerName:
@@ -40,9 +52,8 @@ class TwoDMap(BasePlotter):
             if userPlotDict[key] is not None:
                 plotDict[key] = userPlotDict[key]
 
-
         if plotDict['xextent'] is None:
-            plotDict['xextent'] = [0,metricValue[0,:].size]
+            plotDict['xextent'] = [0, metricValue[0, :].size]
 
         if plotDict['logScale']:
             norm = colors.LogNorm()
@@ -64,8 +75,8 @@ class TwoDMap(BasePlotter):
         image = ax.imshow(metricValue, vmin=plotDict['colorMin'], vmax=plotDict['colorMax'],
                           aspect=plotDict['aspect'], cmap=plotDict['cmap'], norm=norm,
                           extent=extent,
-                          interpolation='none',origin=plotDict['origin'])
-        cb =  plt.colorbar(image)
+                          interpolation='none', origin=plotDict['origin'])
+        cb = plt.colorbar(image)
 
         ax.set_xlabel(plotDict['xlabel'])
         ax.set_ylabel(plotDict['ylabel'])
@@ -79,20 +90,31 @@ class TwoDMap(BasePlotter):
 
 class VisitPairsHist(BasePlotter):
     """
-    Given an opsim2dSlicer, figure out what fraction of observations are in singles, pairs, tripples, etc.
+    Given an opsim2dSlicer, figure out what fraction of observations are in singles, pairs, triples, etc.
     """
     def __init__(self):
         self.plotType = 'TwoD'
         self.objectPlotter = False
-        self.defaultPlotDict = {'title':None, 'xlabel':'N visits per night per field',
-                                'ylabel':'N Visits','label':None,
-                                'logScale':False, 'cbarFormat':None, 'cmap':perceptual_rainbow,
-                                'percentileClip':None, 'colorMin':None, 'colorMax':None,
-                                'zp':None, 'normVal':None,
-                                'cbar_edge':True, 'nTicks':None, 'xlim':[0,20], 'ylim':None}
+        self.defaultPlotDict = {'title': None, 'xlabel': 'N visits per night per field',
+                                'ylabel': 'N Visits', 'label': None, 'color': 'b',
+                                'xlim': [0, 20], 'ylim': None}
 
-    def __call__(self, metricValueIn, slicer, userPlotDict, fignum=None):
+    def __call__(self, metricValue, slicer, userPlotDict, fignum=None):
+        """
+        Parameters
+        ----------
+        metricValue : numpy.ma.MaskedArray
+        slicer : lsst.sims.maf.slicers.TwoDSlicer
+        userPlotDict: dict
+            Dictionary of plot parameters set by user (overrides default values).
+        fignum : int
+            Matplotlib figure number to use (default = None, starts new figure).
 
+        Returns
+        -------
+        int
+           Matplotlib figure number used to create the plot.
+        """
         plotDict = {}
         plotDict.update(self.defaultPlotDict)
         # Don't clobber with None
@@ -100,15 +122,15 @@ class VisitPairsHist(BasePlotter):
             if userPlotDict[key] is not None:
                 plotDict[key] = userPlotDict[key]
 
-        maxVal = metricValueIn.max()
-        bins = np.arange(0.5,maxVal+0.5,1)
+        maxVal = metricValue.max()
+        bins = np.arange(0.5, maxVal + 0.5, 1)
 
-        vals, bins = np.histogram(metricValueIn, bins)
-        xvals = (bins[:-1]+bins[1:])/2.
+        vals, bins = np.histogram(metricValue, bins)
+        xvals = (bins[:-1] + bins[1:]) / 2.
 
         figure = plt.figure(fignum)
         ax = figure.add_subplot(111)
-        ax.bar(xvals, vals*xvals, color=plotDict['color'] )
+        ax.bar(xvals, vals * xvals, color=plotDict['color'], label=plotDict['label'])
         ax.set_xlabel(plotDict['xlabel'])
         ax.set_ylabel(plotDict['ylabel'])
         ax.set_title(plotDict['title'])
