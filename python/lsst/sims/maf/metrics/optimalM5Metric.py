3a7846b66e6cb8356a712b1ad7ccdd73e2393b13
Peter Yoachim <yoachim@uw.edu>
2016-01-28 22:00:21 -0800

    docstring
    more doc and flake8
    
    doc and flake8

--- python/lsst/sims/maf/metrics/optimalM5Metric.py
+++ python/lsst/sims/maf/metrics/optimalM5Metric.py
@@ -4,6 +4,7 @@ import numpy as np
 
 __all__ = ['OptimalM5Metric']
 
+
 class OptimalM5Metric(BaseMetric):
     """
     Compare the co-added depth of the survey to one where
@@ -12,11 +13,37 @@ class OptimalM5Metric(BaseMetric):
 
     def __init__(self, m5Col='fiveSigmaDepth', optM5Col='m5Optimal',
                  normalize=False, **kwargs):
+        """
+        Parameters
+        ----------
+        m5Col : str ('fiveSigmaDepth')
+            Column name that contains the five-sigma limiting depth of
+            each observation
+        optM5Col : str ('m5Optimal')
+            The column name of the five-sigma-limiting depth if the
+            observation had been taken on the meridian.
+        normalize : bool (False)
+            If False, metric returns how many more observations would need
+            to be taken to reach the optimal depth.  If True, the number
+            is normalized by the total number of observations already taken
+            at that position.
+
+        Returns
+        --------
+        out : float
+            If normalize is True, output is the percentage of time the survey
+            is behind an optimal meridian-scanning survey. So if a 10-year
+            survey is at 20%, it would need to run for 12 years to reach the
+            same depth as a 10-year meridian survey.
+            If normalize is False (default), `out` is just the number of
+            additional observations the survey needs to catch up to optimal.
+        """
         if normalize:
             self.units = '% behind'
         else:
             self.units = 'N visits behind'
-        super(OptimalM5Metric, self).__init__(col=[m5Col,optM5Col], units=self.units, **kwargs)
+        super(OptimalM5Metric, self).__init__(col=[m5Col, optM5Col],
+                                              units=self.units, **kwargs)
         self.m5Col = m5Col
         self.optM5Col = optM5Col
         self.normalize = normalize
@@ -24,16 +51,13 @@ class OptimalM5Metric(BaseMetric):
         self.coaddOptimal = Coaddm5Metric(m5Col=optM5Col)
 
     def run(self, dataSlice, slicePoint=None):
-        """
-        Find how many median depth exposures it would take to raise the
-        co-added depth to the optimal meridian co-added depth.
-        """
+
         regularDepth = self.coaddRegular.run(dataSlice)
         optimalDepth = self.coaddOptimal.run(dataSlice)
         medianSingle = np.median(dataSlice[self.m5Col])
 
-        result = (10.**(0.8*optimalDepth)-10.**(0.8*regularDepth))/ \
-                 (10.**(0.8*medianSingle))
+        result = (10.**(0.8 * optimalDepth)-10.**(0.8 * regularDepth)) / \
+                 (10.**(0.8 * medianSingle))
 
         if self.normalize:
             result = result/np.size(dataSlice)*100.
--- python/lsst/sims/maf/stackers/m5OptimalStacker.py
+++ python/lsst/sims/maf/stackers/m5OptimalStacker.py
@@ -11,15 +11,34 @@ class M5OptimalStacker(BaseStacker):
     Make a new m5 column as if observations were taken on the meridian.
     If the moon is up, assume sky brightness stays the same.
 
-    Assume seeing scales as airmass^0.6.
-
+    Assumes seeing scales as airmass^0.6. Uses linear fits for sky and airmass relation.
     """
 
     def __init__(self, airmassCol='airmass', decCol='fieldDec',
-                 skyBrightCol='filtSkyBrightness',seeingCol='FWHMeff',
+                 skyBrightCol='filtSkyBrightness', seeingCol='FWHMeff',
                  filterCol='filter', m5Col ='fiveSigmaDepth',
                  moonAltCol='moonAlt', sunAltCol='sunAlt',
                  site='LSST'):
+        """
+        Parameters
+        ----------
+        airmassCol : str ('airmass')
+            Column name for the airmass per pointing.
+        decCol : str ('fieldDec')
+            Column name for the pointing declination.
+        skyBrightCol: str ('filtSkyBrightness')
+            Column name for the sky brighntess per pointing.
+        filterCol : str ('filter')
+            Column name for the filter name.
+        m5Col : str ('fiveSigmaDepth')
+            Colum name for the five sigma limiting depth per pointing.
+        moonAltCol : str ('moonAlt')
+            Column name for the moon altitude per pointing.
+        sunAltCol : str ('sunAltCol')
+            Column name for the sun altitude column.
+        site : str ('LSST')
+            Name of the site.
+        """
         self.site = Site(site)
         self.units = ['mags']
         self.colsAdded = ['m5Optimal']
@@ -35,12 +54,12 @@ class M5OptimalStacker(BaseStacker):
         self.moonAltCol = moonAltCol
         self.sunAltCol = sunAltCol
 
-    def _run(self,simData):
+    def _run(self, simData):
         # kAtm values from lsst.sims.operations gen_output.py
-        kAtm = {'u':0.50, 'g':0.21, 'r':0.13, 'i':0.10,
-                'z':0.07, 'y':0.18}
+        kAtm = {'u': 0.50, 'g': 0.21, 'r': 0.13, 'i': 0.10,
+                'z': 0.07, 'y': 0.18}
         # linear fits to sky brightness change, no moon, twilight, or zodiacal components
-        skySlopes = {'u':-0.271, 'g':-0.511, 'r':-0.605, 'i':-0.670, 'z':-0.686, 'y':-0.678}
+        skySlopes = {'u': -0.271, 'g': -0.511, 'r': -0.605, 'i': -0.670, 'z': -0.686, 'y': -0.678}
         min_z_possible = np.abs(simData[self.decCol] - self.site.latitude_rad)
         min_airmass_possible = 1./np.cos(min_z_possible)
         for filterName in np.unique(simData[self.filterCol]):
@@ -50,7 +69,7 @@ class M5OptimalStacker(BaseStacker):
             m5Optimal = simData[self.m5Col] - \
                         0.5*deltaSky - \
                         0.15*np.log10(min_airmass_possible / simData[self.airmassCol]) - \
-                        kAtm[filterName]*(min_airmass_possible - simData[self.airmassCol] )
+                        kAtm[filterName]*(min_airmass_possible - simData[self.airmassCol])
             good = np.where(simData[self.filterCol] == filterName)
             simData['m5Optimal'][good] = m5Optimal[good]
         return simData
