a13e8ee46ee746422368d6ff3aea981bfca856ba
Peter Yoachim <yoachim@uw.edu>
2016-02-05 09:36:43 -0800

    flake8

--- python/lsst/sims/maf/maps/createStarDensitymap.py
+++ python/lsst/sims/maf/maps/createStarDensitymap.py
@@ -14,62 +14,58 @@ import argparse
 # Connect to fatboy with: ssh -L 51433:fatboy-private.phys.washington.edu:1433 gateway.astro.washington.edu
 # If non-astro user, use simsuser@gateway.astro.washington.edu
 
-if __name__ =='__main__':
-
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Build a stellar density healpix map")
-    parser.add_argument("filtername", type=str, default='r',help="which filter: u,g,r,i,z,y")
+    parser.add_argument("filtername", type=str, default='r', help="which filter: u, g, r, i, z, y")
 
     args = parser.parse_args()
 
     filterName = args.filtername
     colName = filterName+'mag'
 
-    # Set up healpy map and ra,dec centers
+    # Set up healpy map and ra, dec centers
     nside = 64
-    #nside = 16
-    # set the min to 15 since we saturate there. CatSim max is 28
-    bins = np.arange(15.,28.2,.2)
-    starDensity = np.zeros((hp.nside2npix(nside),np.size(bins)-1), dtype=float)
+
+    # Set the min to 15 since we saturate there. CatSim max is 28
+    bins = np.arange(15., 28.2, .2)
+    starDensity = np.zeros((hp.nside2npix(nside), np.size(bins)-1), dtype=float)
     overMaxMask = np.zeros(hp.nside2npix(nside), dtype=bool)
-    lat, ra = hp.pix2ang(nside,np.arange(0,hp.nside2npix(nside)))
+    lat, ra = hp.pix2ang(nside, np.arange(0, hp.nside2npix(nside)))
     dec = np.pi/2.-lat
 
-
-    # square root of pixel area.
+    # Square root of pixel area.
     hpsizeDeg = hp.nside2resol(nside, arcmin=True)/60.
 
     # Limit things to a 10 arcmin radius
-    hpsizeDeg = np.min([10./60., hpsizeDeg] )
-    #import pdb ; pdb.set_trace()
+    hpsizeDeg = np.min([10./60., hpsizeDeg])
 
-    # options include galaxyBase, cepheidstars, wdstars, rrlystars, msstars, bhbstars, allstars, and more...
+    # Options include galaxyBase, cepheidstars, wdstars, rrlystars, msstars, bhbstars, allstars, and more...
     dbobj = CatalogDBObject.from_objid('allstars')
 
     indxMin = 0
 
-    restoreFile = glob.glob('starDensity_%s_nside_%i.npz' % (filterName,nside))
+    restoreFile = glob.glob('starDensity_%s_nside_%i.npz' % (filterName, nside))
     if len(restoreFile) > 0:
         data = np.load(restoreFile[0])
         starDensity = data['starDensity'].copy()
         indxMin = data['icheck'].copy()
         overMaxMask = data['overMaxMask'].copy()
 
-
     print ''
     # Look at a cirular area the same area as the healpix it's centered on.
     boundLength = hpsizeDeg/np.pi**0.5
 
-    blockArea = hpsizeDeg**2 # sq deg
+    blockArea = hpsizeDeg**2  # sq deg
 
     checksize = 1000
     printsize = 10
-    npix=float(hp.nside2npix(nside))
+    npix = float(hp.nside2npix(nside))
 
     # If the area has more than this number of objects, flag it as a max
     breakLimit = 1e6
-    chunk_size=10000
-    for i in np.arange(indxMin,npix):
+    chunk_size = 10000
+    for i in np.arange(indxMin, npix):
         lastCP = ''
         # wonder what the units of boundLength are...degrees! And it's a radius
         # The newer interface:
@@ -77,7 +73,6 @@ if __name__ =='__main__':
                                            pointingRA=np.degrees(ra[i]),
                                            pointingDec=np.degrees(dec[i]),
                                            boundLength=boundLength, mjd=5700)
-
 
         t = dbobj.getCatalog('ref_catalog_star', obs_metadata=obs_metadata)
 
@@ -90,7 +85,7 @@ if __name__ =='__main__':
         tempHist = np.zeros(np.size(bins)-1, dtype=float)
         counter = 0
         for chunk in chunks:
-            chunkHist,bins = np.histogram(chunk[colName],bins)
+            chunkHist, bins = np.histogram(chunk[colName], bins)
             tempHist += chunkHist
             counter += chunk_size
             if counter >= breakLimit:
@@ -101,17 +96,16 @@ if __name__ =='__main__':
 
         # Checkpoint
         if (i % checksize == 0) & (i != 0):
-            np.savez('starDensity_%s_nside_%i.npz' % (filterName,nside),
+            np.savez('starDensity_%s_nside_%i.npz' % (filterName, nside),
                      starDensity=starDensity, bins=bins, icheck=i, overMaxMask=overMaxMask)
-            lastCP = 'Checkpointed at i=%i of %i' % (i,npix)
+            lastCP = 'Checkpointed at i=%i of %i' % (i, npix)
         if i % printsize == 0:
             sys.stdout.write('\r')
-            perComplete = float(i)/npix*100
-            sys.stdout.write(r'%.2f%% complete. ' %(perComplete) + lastCP)
+            perComplete = float(i) / npix * 100
+            sys.stdout.write(r'%.2f%% complete. ' % (perComplete) + lastCP)
             sys.stdout.flush()
 
-
-    np.savez('starDensity_%s_nside_%i.npz' % (filterName,nside), starDensity=starDensity,
-             bins=bins,overMaxMask=overMaxMask )
+    np.savez('starDensity_%s_nside_%i.npz' % (filterName, nside), starDensity=starDensity,
+             bins=bins, overMaxMask=overMaxMask)
     print ''
     print 'Completed!'
