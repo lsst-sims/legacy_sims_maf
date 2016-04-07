0c1c370650676aaa13d2f0b7ceb61794739d7074
Peter Yoachim <yoachim@uw.edu>
2016-04-07 15:49:31 -0700

    tests pass

--- tests/testUtils.py
+++ tests/testUtils.py
@@ -1,23 +1,10 @@
 import matplotlib
 matplotlib.use("Agg")
 import unittest
-import numpy as np
 
 import lsst.sims.maf.utils as utils
 
 class TestUtils(unittest.TestCase):
-
-    def testStellarMags(self):
-        """
-        Test that the stellar magnitudes are created for accepted types.
-        """
-        stellarTypes = ['O','B','A','F','G','K','M']
-        for st in stellarTypes:
-            mags = utils.stellarMags(st)
-            mags2 = utils.stellarMags(st, rmag=20.)
-            for key in mags:
-                self.assertLess(mags[key], mags2[key])
-        self.assertRaises(ValueError, utils.stellarMags, 'ack')
 
     def testCreateSqlWhere(self):
         """
