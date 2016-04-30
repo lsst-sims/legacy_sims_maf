68cec88670223573fb510c421b65c6c972c9ce2a
Lynne Jones <lynnej@uw.edu>
2016-04-30 03:07:47 -0700

    Let plot make a heavier line at Hmark.

--- python/lsst/sims/maf/plots/moPlotters.py
+++ python/lsst/sims/maf/plots/moPlotters.py
@@ -20,8 +20,9 @@ class MetricVsH(BasePlotter):
     def __init__(self):
         self.plotType = 'MetricVsH'
         self.objectPlotter = False
-        self.defaultPlotDict = {'title':None, 'xlabel':'H (mag)', 'ylabel':None, 'label':None,
-                                'npReduce':None, 'nbins':None, 'albedo':None, 'Hmark':None}
+        self.defaultPlotDict = {'title': None, 'xlabel': 'H (mag)', 'ylabel': None, 'label': None,
+                                'npReduce': None, 'nbins': None, 'albedo': None,
+                                'Hmark': None, 'HmarkLinestyle': ':'}
         self.minHrange=1.0
 
     def __call__(self, metricValue, slicer, userPlotDict, fignum=None):
@@ -95,7 +96,8 @@ class MetricVsH(BasePlotter):
             y = 1.1
         plt.grid(True)
         if plotDict['Hmark'] is not None:
-            plt.axvline(x=plotDict['Hmark'], color='r', linestyle=':', alpha=0.3)
+            plt.axvline(x=plotDict['Hmark'], color='r',
+                        linestyle=plotDict['HmarkLinestyle'], alpha=0.3)
         plt.title(plotDict['title'], y=y)
         plt.xlabel(plotDict['xlabel'])
         plt.ylabel(plotDict['ylabel'])
