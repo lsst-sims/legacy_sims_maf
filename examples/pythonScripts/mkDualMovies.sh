a329598b1e160b264c3456631e4de2bba7346d97
Lynne Jones <lynnej@uw.edu>
2017-03-01 18:27:23 -0800

    Make the example movie / visits dual movie work.

--- examples/pythonScripts/mkDualMovies.sh
+++ examples/pythonScripts/mkDualMovies.sh
@@ -8,14 +8,15 @@
 
 set opsimRun = $1
 set sqlconstraint = 'filter="r"'
+set sqlconstraint = ''
 
-echo "python example_movie.py "$opsimRun"_sqlite.db --movieStepsize 30 --nside 64 --ips 5 --sqlConstraint '"$sqlconstraint"' --raCol ditheredRA --decCol ditheredDec --outDir "$opsimRun"_r_cumulative"
+echo "python example_movie.py "$opsimRun".db --movieStepsize 30 --nside 32 --ips 5 --sqlConstraint '"$sqlconstraint"' --outDir "$opsimRun"_cumulative"
 
-echo "python example_movie.py "$opsimRun"_sqlite.db --movieStepsize 30 --nside 64 --ips 5 --sqlConstraint '"$sqlconstraint"' --raCol ditheredRA --decCol ditheredDec --binned --outDir "$opsimRun"_r_binned"
+echo "python example_movie.py "$opsimRun".db --movieStepsize 30 --nside 32 --ips 5 --sqlConstraint '"$sqlconstraint"' --binned --outDir "$opsimRun"_binned"
 
-echo "ffmpeg -i "$opsimRun"_r_cumulative/"$opsimRun"_N_Visits_HEAL_SkyMap_5.0_5.0.mp4 -i "$opsimRun"_r_binned/"$opsimRun"_N_Visits_HEAL_SkyMap_5.0_5.0.mp4 -filter_complex 'nullsrc=size=1224x388 [background]; [0:v] setpts=PTS-STARTPTS [left]; [1:v] setpts=PTS-STARTPTS [right]; [background][left] overlay=shortest=1 [background+left]; [background+left][right] overlay=shortest=1:x=612 [left+right]' -map '[left+right]' -r 5  -pix_fmt yuv420p "$opsimRun"_r_N_Visits.mp4"
+echo "ffmpeg -i "$opsimRun"_cumulative/"$opsimRun"_N_Visits_HEAL_SkyMap_5.0_5.0.mp4 -i "$opsimRun"_binned/"$opsimRun"_N_Visits_HEAL_SkyMap_5.0_5.0.mp4 -filter_complex 'nullsrc=size=1152x576 [background]; [0:v] setpts=PTS-STARTPTS [left]; [1:v] setpts=PTS-STARTPTS [right]; [background][left] overlay=shortest=1 [background+left]; [background+left][right] overlay=shortest=1:x=576 [left+right]' -map '[left+right]' -r 5  -pix_fmt yuv420p "$opsimRun"_N_Visits.mp4"
 
-echo "ffmpeg -i "$opsimRun"_r_cumulative/"$opsimRun"_Coaddm5Metric_HEAL_SkyMap_5.0_5.0.mp4 -i "$opsimRun"_r_binned/"$opsimRun"_Coaddm5Metric_HEAL_SkyMap_5.0_5.0.mp4 -filter_complex 'nullsrc=size=1224x388 [background]; [0:v] setpts=PTS-STARTPTS [left]; [1:v] setpts=PTS-STARTPTS [right]; [background][left] overlay=shortest=1 [background+left]; [background+left][right] overlay=shortest=1:x=612 [left+right]' -map '[left+right]' -r 5 -pix_fmt yuv420p "$opsimRun"_r_Coaddm5.mp4"
+echo "ffmpeg -i "$opsimRun"_cumulative/"$opsimRun"_Coaddm5Metric_HEAL_SkyMap_5.0_5.0.mp4 -i "$opsimRun"_binned/"$opsimRun"_Coaddm5Metric_HEAL_SkyMap_5.0_5.0.mp4 -filter_complex 'nullsrc=size=1224x388 [background]; [0:v] setpts=PTS-STARTPTS [left]; [1:v] setpts=PTS-STARTPTS [right]; [background][left] overlay=shortest=1 [background+left]; [background+left][right] overlay=shortest=1:x=612 [left+right]' -map '[left+right]' -r 5 -pix_fmt yuv420p "$opsimRun"_Coaddm5.mp4"
 
 
 
