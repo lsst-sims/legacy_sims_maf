02f963ba81068ef8ee65a24fdb9cbc1bf9a9b65f
Lynne Jones <lynnej@uw.edu>
2017-03-02 23:07:16 -0800

    Tweak back to v3.

--- examples/pythonScripts/mkDualMovies.sh
+++ examples/pythonScripts/mkDualMovies.sh
@@ -10,9 +10,9 @@ set opsimRun = $1
 set sqlconstraint = 'filter="r"'
 set sqlconstraint = ''
 
-echo "python example_movie.py "$opsimRun".db --movieStepsize 30 --nside 32 --ips 5 --sqlConstraint '"$sqlconstraint"' --outDir "$opsimRun"_cumulative"
+echo "python example_movie.py "$opsimRun"_sqlite.db --movieStepsize 30 --nside 32 --ips 5 --sqlConstraint '"$sqlconstraint"' --outDir "$opsimRun"_cumulative"
 
-echo "python example_movie.py "$opsimRun".db --movieStepsize 30 --nside 32 --ips 5 --sqlConstraint '"$sqlconstraint"' --binned --outDir "$opsimRun"_binned"
+echo "python example_movie.py "$opsimRun"_sqlite.db --movieStepsize 30 --nside 32 --ips 5 --sqlConstraint '"$sqlconstraint"' --binned --outDir "$opsimRun"_binned"
 
 echo "ffmpeg -i "$opsimRun"_cumulative/"$opsimRun"_N_Visits_HEAL_SkyMap_5.0_5.0.mp4 -i "$opsimRun"_binned/"$opsimRun"_N_Visits_HEAL_SkyMap_5.0_5.0.mp4 -filter_complex 'nullsrc=size=1152x576 [background]; [0:v] setpts=PTS-STARTPTS [left]; [1:v] setpts=PTS-STARTPTS [right]; [background][left] overlay=shortest=1 [background+left]; [background+left][right] overlay=shortest=1:x=576 [left+right]' -map '[left+right]' -r 5  -pix_fmt yuv420p "$opsimRun"_N_Visits.mp4"
 
