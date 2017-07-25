111379dc351941d5ce320326fe1bbfccec4807ae
Lynne Jones <lynnej@uw.edu>
2017-07-25 16:59:31 -0700

    Add AbsMaxPercentMetric, to use for slew speed evaluation

--- python/lsst/sims/maf/metrics/simpleMetrics.py
+++ python/lsst/sims/maf/metrics/simpleMetrics.py
@@ -3,10 +3,10 @@ from .baseMetric import BaseMetric
 
 # A collection of commonly used simple metrics, operating on a single column and returning a float.
 
-__all__ = ['PassMetric', 'Coaddm5Metric', 'MaxMetric', 'MeanMetric', 'MedianMetric', 'MedianAbsMetric',
+__all__ = ['PassMetric', 'Coaddm5Metric', 'MaxMetric', 'MeanMetric', 'MedianMetric', 'AbsMedianMetric',
            'MinMetric', 'FullRangeMetric', 'RmsMetric', 'SumMetric', 'CountUniqueMetric',
            'CountMetric', 'CountRatioMetric', 'CountSubsetMetric', 'RobustRmsMetric',
-           'MaxPercentMetric', 'BinaryMetric', 'FracAboveMetric', 'FracBelowMetric',
+           'MaxPercentMetric', 'AbsMaxPercentMetric', 'BinaryMetric', 'FracAboveMetric', 'FracBelowMetric',
            'PercentileMetric', 'NoutliersNsigmaMetric', 'UniqueRatioMetric',
            'MeanAngleMetric', 'RmsAngleMetric', 'FullRangeAngleMetric']
 
@@ -25,7 +25,8 @@ class PassMetric(BaseMetric):
         return dataSlice
 
 class Coaddm5Metric(BaseMetric):
-    """Calculate the coadded m5 value at this gridpoint."""
+    """Calculate the coadded m5 value at this gridpoint.
+    """
     def __init__(self, m5Col = 'fiveSigmaDepth', metricName='CoaddM5', **kwargs):
         """Instantiate metric.
 
@@ -35,52 +36,62 @@ class Coaddm5Metric(BaseMetric):
         return 1.25 * np.log10(np.sum(10.**(.8*dataSlice[self.colname])))
 
 class MaxMetric(BaseMetric):
-    """Calculate the maximum of a simData column slice."""
+    """Calculate the maximum of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.max(dataSlice[self.colname])
 
 class MeanMetric(BaseMetric):
-    """Calculate the mean of a simData column slice."""
+    """Calculate the mean of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.mean(dataSlice[self.colname])
 
 class MedianMetric(BaseMetric):
-    """Calculate the median of a simData column slice."""
+    """Calculate the median of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.median(dataSlice[self.colname])
 
-class MedianAbsMetric(BaseMetric):
-    """Calculate the median of the absolute value of a simData column slice."""
+class AbsMedianMetric(BaseMetric):
+    """Calculate the median of the absolute value of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.median(np.abs(dataSlice[self.colname]))
 
 class MinMetric(BaseMetric):
-    """Calculate the minimum of a simData column slice."""
+    """Calculate the minimum of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.min(dataSlice[self.colname])
 
 class FullRangeMetric(BaseMetric):
-    """Calculate the range of a simData column slice."""
+    """Calculate the range of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.max(dataSlice[self.colname])-np.min(dataSlice[self.colname])
 
 class RmsMetric(BaseMetric):
-    """Calculate the standard deviation of a simData column slice."""
+    """Calculate the standard deviation of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.std(dataSlice[self.colname])
 
 class SumMetric(BaseMetric):
-    """Calculate the sum of a simData column slice."""
+    """Calculate the sum of a simData column slice.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.sum(dataSlice[self.colname])
 
 class CountUniqueMetric(BaseMetric):
-    """Return the number of unique values """
+    """Return the number of unique values.
+    """
     def run(self, dataSlice, slicePoint=None):
         return np.size(np.unique(dataSlice[self.colname]))
 
 class UniqueRatioMetric(BaseMetric):
-    """Return the number of unique values divided by the total"""
+    """Return the number of unique values divided by the total number of values.
+    """
     def run(self, dataSlice, slicePoint=None):
         ntot = float(np.size(dataSlice[self.colname]))
         result = np.size(np.unique(dataSlice[self.colname])) / ntot
@@ -96,7 +107,8 @@ class CountMetric(BaseMetric):
         return len(dataSlice[self.colname])
 
 class CountRatioMetric(BaseMetric):
-    """Count the length of a simData column slice, then divide by 'normVal'. """
+    """Count the length of a simData column slice, then divide by 'normVal'. 
+    """
     def __init__(self, col=None, normVal=1., metricName=None, **kwargs):
         self.normVal = float(normVal)
         if metricName is None:
@@ -107,7 +119,8 @@ class CountRatioMetric(BaseMetric):
         return len(dataSlice[self.colname])/self.normVal
 
 class CountSubsetMetric(BaseMetric):
-    """Count the length of a simData column slice which matches 'subset'. """
+    """Count the length of a simData column slice which matches 'subset'. 
+    """
     def __init__(self, col=None, subset=None, **kwargs):
         super(CountSubsetMetric, self).__init__(col=col, **kwargs)
         self.metricDtype = 'int'
@@ -119,22 +132,34 @@ class CountSubsetMetric(BaseMetric):
         return count
 
 class RobustRmsMetric(BaseMetric):
-    """Use the inter-quartile range of the data to estimate the RMS.  Robust since this calculation
-    does not include outliers in the distribution"""
+    """Use the inter-quartile range of the data to estimate the RMS.  
+    Robust since this calculation does not include outliers in the distribution.
+    """
     def run(self, dataSlice, slicePoint=None):
         iqr = np.percentile(dataSlice[self.colname],75)-np.percentile(dataSlice[self.colname],25)
         rms = iqr/1.349 #approximation
         return rms
 
 class MaxPercentMetric(BaseMetric):
-    """Return the percent of the data which has the maximum value."""
+    """Return the percent of the data which has the maximum value.
+    """
     def run(self, dataSlice, slicePoint=None):
         nMax = np.size(np.where(dataSlice[self.colname] == np.max(dataSlice[self.colname]))[0])
-        percent = nMax/float(dataSlice[self.colname].size)*100.
+        percent = nMax / float(dataSlice[self.colname].size) * 100.
+        return percent
+
+class AbsMaxPercentMetric(BaseMetric):
+    """Return the percent of the data which has the absolute value of the max value of the data.
+    """
+    def run(self, dataSlice, slicePoint=None):
+        maxVal = np.abs(np.max(dataSlice[self.colname]))
+        nMax = np.size(np.where(np.abs(dataSlice[self.colname]) == maxVal)[0])
+        percent = nMax / float(dataSlice[self.colname].size) * 100.0
         return percent
 
 class BinaryMetric(BaseMetric):
-    """Return 1 if there is data. """
+    """Return 1 if there is data. 
+    """
     def run(self, dataSlice, slicePoint=None):
         if dataSlice.size > 0:
             return 1
@@ -142,6 +167,8 @@ class BinaryMetric(BaseMetric):
             return self.badval
 
 class FracAboveMetric(BaseMetric):
+    """Find the fraction of data values above a given value.
+    """
     def __init__(self, col=None, cutoff=0.5, scale=1, metricName=None, **kwargs):
         # Col could just get passed in bundle with kwargs, but by explicitly pulling it out
         #  first, we support use cases where class instantiated without explicit 'col=').
@@ -157,6 +184,8 @@ class FracAboveMetric(BaseMetric):
         return fracAbove
 
 class FracBelowMetric(BaseMetric):
+    """Find the fraction of data values below a given value.
+    """
     def __init__(self, col=None, cutoff=0.5, scale=1, metricName=None, **kwargs):
         if metricName is None:
             metricName = 'FracBelow %.2f %s' %(cutoff, col)
@@ -170,6 +199,8 @@ class FracBelowMetric(BaseMetric):
         return fracBelow
 
 class PercentileMetric(BaseMetric):
+    """Find the value of a column at a given percentile.
+    """
     def __init__(self, col=None, percentile=90, metricName=None, **kwargs):
         if metricName is None:
             metricName = '%.0fth%sile %s' %(percentile, '%', col)
@@ -180,8 +211,7 @@ class PercentileMetric(BaseMetric):
         return pval
 
 class NoutliersNsigmaMetric(BaseMetric):
-    """
-    Calculate the # of visits less than nSigma below the mean (nSigma<0) or
+    """Calculate the # of visits less than nSigma below the mean (nSigma<0) or
     more than nSigma above the mean of 'col'.
     """
     def __init__(self, col=None, nSigma=3., metricName=None, **kwargs):
@@ -230,11 +260,13 @@ def _rotateAngles(angles):
 class MeanAngleMetric(BaseMetric):
     """Calculate the mean of an angular (degree) simData column slice.
 
-    'MeanAngle' differs from 'Mean' in that it accounts for wraparound at 2pi."""
+    'MeanAngle' differs from 'Mean' in that it accounts for wraparound at 2pi.
+    """
     def run(self, dataSlice, slicePoint=None):
         """Calculate mean angle via unit vectors.
         If unit vector 'strength' is less than 0.1, then just set mean to 180 degrees
-        (as this indicates nearly uniformly distributed angles). """
+        (as this indicates nearly uniformly distributed angles). 
+        """
         x = np.cos(np.radians(dataSlice[self.colname]))
         y = np.sin(np.radians(dataSlice[self.colname]))
         meanx = np.mean(x)
@@ -249,7 +281,8 @@ class MeanAngleMetric(BaseMetric):
 class RmsAngleMetric(BaseMetric):
     """Calculate the standard deviation of an angular (degrees) simData column slice.
 
-    'RmsAngle' differs from 'Rms' in that it accounts for wraparound at 2pi."""
+    'RmsAngle' differs from 'Rms' in that it accounts for wraparound at 2pi.
+    """
     def run(self, dataSlice, slicePoint=None):
         rotation, angles = _rotateAngles(np.radians(dataSlice[self.colname]))
         return np.std(np.degrees(angles))
@@ -257,7 +290,8 @@ class RmsAngleMetric(BaseMetric):
 class FullRangeAngleMetric(BaseMetric):
     """Calculate the full range of an angular (degrees) simData column slice.
 
-    'FullRangeAngle' differs from 'FullRange' in that it accounts for wraparound at 2pi."""
+    'FullRangeAngle' differs from 'FullRange' in that it accounts for wraparound at 2pi.
+    """
     def run(self, dataSlice, slicePoint=None):
         rotation, angles = _rotateAngles(np.radians(dataSlice[self.colname]))
         return np.degrees(angles.max() - angles.min())
