f6378884507e9b0929a64bc7a5eb63616a2a995b
Lynne Jones <lynnej@uw.edu>
2017-09-29 12:07:52 -0700

    Updated automatic caption generation

--- python/lsst/sims/maf/metricBundles/metricBundle.py
+++ python/lsst/sims/maf/metricBundles/metricBundle.py
@@ -324,8 +324,11 @@ class MetricBundle(object):
         # If we still need to auto-generate a caption, do it.
         if self.displayDict['caption'] is None:
             if self.metric.comment is None:
-                caption = self.metric.name + ' calculated on a %s' % (self.slicer.slicerName)
-                caption += ' basis, using a subset of data selected via %s.' % (self.constraint)
+                caption = self.metric.name + ' calculated on a %s basis' % (self.slicer.slicerName)
+                if self.constraint!='' and self.constraint is not None:
+                    caption += ' using a subset of data selected via %s.' % (self.constraint)
+                else:
+                    caption += '.'
             else:
                 caption = self.metric.comment
             if 'zp' in self.plotDict:
