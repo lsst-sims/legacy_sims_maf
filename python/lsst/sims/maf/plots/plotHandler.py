5aac2953fd397ddf56ddd32f93b3e14b78858a84
Lynne Jones <lynnej@uw.edu>
2017-12-07 10:52:39 -0800

    Remove tight_layout option as it wasn't helping

--- python/lsst/sims/maf/plots/plotHandler.py
+++ python/lsst/sims/maf/plots/plotHandler.py
@@ -572,8 +572,6 @@ class PlotHandler(object):
         # Add the super title if provided.
         if 'suptitle' in self.plotDicts[0]:
             plt.suptitle(self.plotDicts[0]['suptitle'])
-        # Set up tight layout.
-        plt.tight_layout()
         # Save to disk and file info to resultsDb if desired.
         if self.savefig:
             if displayDict is None:
