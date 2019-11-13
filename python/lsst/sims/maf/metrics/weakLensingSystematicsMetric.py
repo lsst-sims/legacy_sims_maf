d8f9e1309c5b651572ec56ef7ce5cca6dbd7ae7f
Husni Almoubayyed <h.almoubayyed@gmail.com>
2019-11-13 17:11:46 -0500

    Adding WL systematics metric (#168)
    * adding weak lensing systematics metric from white paper
    
    * adding weak lensing systematics metric
    
    * updating metric to use a healpix grid
    
    now calculating total number of visits, will add a summary metric to calculate the average
    
    * removing unnecessary code, cleaning up
    
    removed the code that was checking whether an observation is within 1.75 degrees from the healpix point which seemed to be causing wrong answers; healpix slicer should be used instead. removed some other unnecessary code throughout and added more comments in docstring.
    
    * renamed the metric
    
    * switched to relative imports
    
    * changing metric name, removing unneeded columns
    
    * fixed a typo
    
    * made ebv a parameter instead of hardcoded

---
+++ python/lsst/sims/maf/metrics/weakLensingSystematicsMetric.py
@@ -0,0 +1,54 @@
+from .baseMetric import BaseMetric
+from .exgalM5 import ExgalM5
+from ..maps import DustMap
+
+
+__all__ = ['WeakLensingNvisits']
+
+class WeakLensingNvisits(BaseMetric):
+    """Note:
+        Should be run with the HealpixSlicer. If using dithering (which
+        should be the case unless dithering is already implemented in the run)
+        then should be run with a stacker and appropriate column names for 
+        dithered RA and Dec should be provided.
+    """
+
+    
+    def __init__(self,
+                 maps,
+                 depthlim=24.5,
+                 ebvlim=0.2,
+                 metricName='WeakLensingNvisits',
+                 **kwargs):
+        """Weak Lensing systematics metric
+
+        Computes the average number of visits per point on a HEALPix grid
+        after a maximum E(B-V) cut and a minimum co-added depth cut.
+        """
+        
+        super().__init__(
+            metricName=metricName, 
+            col=['fiveSigmaDepth'],
+            maps=maps, 
+            **kwargs
+            )
+        self.ExgalM5 = ExgalM5()
+        self.depthlim = depthlim
+        self.ebvlim = ebvlim
+
+    def run(self, dataSlice, slicePoint=None):
+        """runs the metric
+
+        Args:
+            dataSlice (ndarray): positional data from querying the database
+            slicePoint (dict): queried data along with data from stackers
+        Returns:
+            the number of visits that can observe this healpix point.
+        """
+        if slicePoint['ebv'] > self.ebvlim:
+            return self.badval
+        ExgalM5 = self.ExgalM5.run(dataSlice=dataSlice, slicePoint=slicePoint)
+        if ExgalM5 < self.depthlim:
+            return self.badval
+        return len(dataSlice)
+
