9eefde57fa7a306e717560db14b57779df5c9fd7
Lynne Jones <lynnej@uw.edu>
2020-04-06 14:01:38 -0700

    Update movie scripts and put in new logos

--- movies/joinOpsMovieFull.sh
+++ movies/joinOpsMovieFull.sh
@@ -9,12 +9,13 @@
 ##  blank space between each movie night. Adding more versions of 'blank.mp4' will add a longer pause.
 ## Note that the bigMovieList file could be edited and the ffmpeg command rerun to change these pauses, etc.
 
-## The 'blank.mp4' file can be re-generated (if a different size is needed, for example) by combining a blank png
-##  from matplotlib with a blank automatically generated frame from ffmpeg (optionally, including the logo).
-## ffmpeg -f lavfi -i color=c=white:s=965x648:d=0.7 -i blank_1.png -i lsstLogo.png \
-## -filter_complex "[0:v] setpts=PTS-STARTPTS [background]; [1:v] setpts=PTS-STARTPTS [blank]; \
-## [2:v] setpts=PTS-STARTPTS,scale=150:-1 [logo]; [background][blank] overlay=shortest=0 [bgd]; \
-## [bgd][logo] overlay=shortest=0:x=10:y=10" -r 30 -pix_fmt yuv420p -crf 18 -preset slower blank2.mp4
+## The 'blank.mp4' file can be re-generated (if a different size is needed, for example) by combining the logos
+##  with a blank automatically generated frame from ffmpeg --
+## ffmpeg -f lavfi -i color=c=white:s=965x648:d=0.7 -i lsstLogo.png -i rubinLogo.png \
+## -filter_complex "[0:v] setpts=PTS-STARTPTS [background]; \
+## [1:v] setpts=PTS-STARTPTS,scale=200:-1 [lsstlogo]; [2:v] setpts=PTS-STARTPTS,scale=250:-1 [rubinlogo];
+## [background][lsstlogo] overlay=shortest=0:x=20:y=20 [tmp1]; [tmp1][rubinlogo] overlay=shortest=0:x=20:y=595"
+## -r 30 -pix_fmt yuv420p -crf 18 -preset slower blank2.mp4
 
 ## usage: expects to be run after mkOpsMovie.sh is used to generate movies for a series of individual nights.
 ## usage: joinOpsMovieFull.sh [opsim name] [start night] [end night]
@@ -36,21 +37,24 @@ if (-e $comboCommand) then
   rm $comboCommand
 endif
 
+echo $nights
 foreach night ($nights)
- if (-e $opsRun"_n"$night"/FilterColors_SkyMap_30.0_30.0.mp4") then
+ if (-e "n"$night"/FilterColors_SkyMap"*".mp4") then
+    echo $night
     # combine NVisits and FilterColors
     # "normal" opsim movie size is 576x432
     echo 'ffmpeg -f lavfi -i color=c=white:s=965x648 \' >> $comboCommand
     echo ' -f lavfi -i color=c=black:s=2x648\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/FilterColors_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/Nvisits_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/Nvisits_u_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/Nvisits_g_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/Nvisits_r_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/Nvisits_i_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/Nvisits_z_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i '$opsRun'_n'$night'/Nvisits_y_SkyMap_30.0_30.0.mp4\'  >> $comboCommand
-    echo ' -i lsstLogo.png\'  >> $comboCommand
+    echo ' -i n'$night'/FilterColors_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i n'$night'/Nvisits_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i n'$night'/Nvisits_u_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i n'$night'/Nvisits_g_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i n'$night'/Nvisits_r_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i n'$night'/Nvisits_i_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i n'$night'/Nvisits_z_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i n'$night'/Nvisits_y_SkyMap_30.0_30.mp4\'  >> $comboCommand
+    echo ' -i lsstLogo.png\' >> $comboCommand
+    echo ' -i rubinLogo.png\'  >> $comboCommand
     echo ' -filter_complex "[0:v] setpts=PTS-STARTPTS [background];\'  >> $comboCommand
     echo ' [1:v] setpts=PTS-STARTPTS [stripe];\'  >> $comboCommand
     echo ' [2:v] setpts=PTS-STARTPTS [pointings];\'  >> $comboCommand
@@ -61,7 +65,8 @@ foreach night ($nights)
     echo ' [7:v] setpts=PTS-STARTPTS,scale=192:-1 [i];\'  >> $comboCommand
     echo ' [8:v] setpts=PTS-STARTPTS,scale=192:-1 [z];\'  >> $comboCommand
     echo ' [9:v] setpts=PTS-STARTPTS,scale=192:-1 [y];\'  >> $comboCommand
-    echo ' [10:v] setpts=PTS-STARTPTS,scale=150:-1 [logo];\'  >> $comboCommand
+    echo ' [10:v] setpts=PTS-STARTPTS,scale=200:-1 [lsstlogo];\'  >> $comboCommand
+    echo ' [11:v] setpts=PTS-STARTPTS,scale=250:-1 [rubinlogo];\' >> $comboCommand
     echo ' [background][stripe] overlay=x=581:y=0 [bgd];\'  >> $comboCommand
     echo ' [bgd][pointings] overlay=shortest=1:x=0:y=100 [tmp1];\'  >> $comboCommand
     echo ' [tmp1][cumulative] overlay=shortest=1:x=630:y=0 [tmp2];\'  >> $comboCommand
@@ -71,9 +76,10 @@ foreach night ($nights)
     echo ' [tmp5][i] overlay=shortest=1:x=775:y=344 [tmp6];\'  >> $comboCommand
     echo ' [tmp6][z] overlay=shortest=1:x=583:y=488 [tmp8];\'  >> $comboCommand
     echo ' [tmp8][y] overlay=shortest=1:x=775:y=488 [tmp9];\'  >> $comboCommand
-    echo ' [tmp9][logo] overlay=shortest=0:x=10:y=10"\'  >> $comboCommand
-    echo ' -r 30 -pix_fmt yuv420p -crf 18 -preset slower '$opsRun'_n'$night'/combo_30.0_30.0.mp4' >> $comboCommand
-    echo 'file '$opsRun'_n'$night'/combo_30.0_30.0.mp4' >> $combomovieList
+    echo ' [tmp9][lsstlogo] overlay=shortest=0:x=20:y=20 [tmp10];\'  >> $comboCommand
+    echo ' [tmp10][rubinlogo] overlay=shortest=0:x=20:y=595"\' >> $comboCommand
+    echo ' -r 30 -pix_fmt yuv420p -crf 18 -preset slower n'$night'/combo_30.0_30.mp4' >> $comboCommand
+    echo 'file n'$night'/combo_30.0_30.mp4' >> $combomovieList
     echo 'file blank2.mp4' >> $combomovieList
  endif
 end
