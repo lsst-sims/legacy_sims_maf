306627ceb7e4cd24ad934d7560013ef583482fa1
pgris <gris@clermont.in2p3.fr>
2020-06-08 08:53:28 +0200

    code cleaning plus selection criteria tuning

--- python/lsst/sims/maf/metrics/snNSNMetric.py
+++ python/lsst/sims/maf/metrics/snNSNMetric.py
@@ -147,7 +147,6 @@ class SNNSNMetric(BaseMetric):
 
         """
         idarray = None
-        healpixID_ref = 1
         healpixID = -1
         if slicePoint is not None:
             if 'nside' in slicePoint.keys():
@@ -159,9 +158,6 @@ class SNNSNMetric(BaseMetric):
 
                 healpixID = hp.ang2pix(
                     slicePoint['nside'], np.rad2deg(slicePoint['ra']), np.rad2deg(slicePoint['dec']), nest=True, lonlat=True)
-                print('healpixID', healpixID)
-                if healpixID == healpixID_ref:
-                    print('point', slicePoint, healpixID)
                 for kk, vv in slicePoint.items():
                     r.append(vv)
                     names.append(kk)
@@ -173,15 +169,7 @@ class SNNSNMetric(BaseMetric):
         dataSlice = self.coadd(pd.DataFrame(dataSlice))
 
         dataSlice = self.getseason(dataSlice)
-        if healpixID == healpixID_ref:
-            print('dataSlice here', dataSlice, dataSlice.dtype)
 
-        """
-        print('after season', dataSlice, len(dataSlice))
-        for seas in np.unique(dataSlice['season']):
-            iu = dataSlice['season'] == seas
-            print(seas, len(dataSlice[iu]))
-        """
         # get the seasons
         seasons = self.season
 
@@ -242,9 +230,6 @@ class SNNSNMetric(BaseMetric):
         # final result: median zlim for a faint sn
         # and nsn_med for z<zlim
 
-        if healpixID == healpixID_ref:
-            print('result here', resdf)
-
         if resdf.empty:
             return nlr.merge_arrays([idarray, self.bad], flatten=True)
 
@@ -262,8 +247,6 @@ class SNNSNMetric(BaseMetric):
 
             resd = np.rec.fromrecords([(nSN, zlim, healpixID)], names=[
                                       'nSN', 'zlim', 'healpixID'])
-            if healpixID == healpixID_ref:
-                print('final result', resd)
             res = nlr.merge_arrays([idarray, resd], flatten=True)
 
         else:
@@ -510,7 +493,7 @@ class SNNSNMetric(BaseMetric):
         idx &= np.abs(effi_seasondf['color']-colorref) < 1.e-5
         sel = effi_seasondf[idx]
 
-        if np.mean(sel['effi']) > 0.10:
+        if np.mean(sel['effi']) > 0.02:
                 # estimate zlims
             zlimsdf = self.zlims(effi_seasondf, dur_z, groupnames)
 
