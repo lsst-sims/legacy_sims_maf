10fee8811fc2c23f448a95e96130b425b2270c20
Lynne Jones <lynnej@uw.edu>
2021-02-16 13:51:24 -0800

    Update SaturationStacker, post exposure time fixes

--- python/lsst/sims/maf/stackers/generalStackers.py
+++ python/lsst/sims/maf/stackers/generalStackers.py
@@ -16,17 +16,17 @@ __all__ = ['NormAirmassStacker', 'ParallaxFactorStacker', 'HourAngleStacker',
 # Season stacker by Phil Marshall (dr.phil.marshall@gmail.com),
 # modified by Humna Awan (humna.awan@rutgers.edu)
 
-
+    
 class SaturationStacker(BaseStacker):
     """Calculate the saturation limit of a point source. Assumes Guassian PSF.
-
+    
     Parameters
     ----------
     pixscale : float, opt (0.2)
-        Acrsec per pixel
+        Arcsec per pixel
     gain : float, opt (2.3)
         electrons per adu
-    saturation_e : float, opt (90e3)
+    saturation_e : float, opt (150e3)
         The saturation level in electrons
     zeropoints : dict-like, opt (None)
         The zeropoints for the telescope. Keys should be str with filter names, values in mags.
@@ -39,7 +39,7 @@ class SaturationStacker(BaseStacker):
     def __init__(self, seeingCol='seeingFwhmEff', skybrightnessCol='skyBrightness',
                  exptimeCol='visitExposureTime', nexpCol='numExposures',
                  filterCol='filter', airmassCol='airmass',
-                 saturation_e=90e3, zeropoints=None, km=None, pixscale=0.2, gain=2.3):
+                 saturation_e=150e3, zeropoints=None, km=None, pixscale=0.2, gain=1.0):
         self.units = ['mag']
         self.colsReq = [seeingCol, skybrightnessCol, exptimeCol, nexpCol, filterCol, airmassCol]
         self.seeingCol = seeingCol
@@ -53,6 +53,7 @@ class SaturationStacker(BaseStacker):
         names = ['u', 'g', 'r', 'i', 'z', 'y']
         types = [float]*6
         if zeropoints is None:
+            # Note these zeropoints are calculating the number of *electrons* per second (thus gain=1)
             # https://github.com/lsst-pst/syseng_throughputs/blob/master/notebooks/Syseng%20Throughputs%20Repo%20Demo.ipynb
             self.zeropoints = np.array([27.03, 28.38, 28.15, 27.86, 27.46, 26.68]).view(list(zip(names, types)))
         else:
@@ -67,14 +68,20 @@ class SaturationStacker(BaseStacker):
     def _run(self, simData, cols_present=False):
         for filtername in np.unique(simData[self.filterCol]):
             in_filt = np.where(simData[self.filterCol] == filtername)[0]
+            # Calculate the length of the on-sky time per EXPOSURE
+            exptime = simData[self.exptimeCol][in_filt] / simData[self.nexpCol][in_filt]
+            # Calculate sky counts per pixel per second from skybrightness + zeropoint (e/1s)
             sky_counts = 10.**(0.4*(self.zeropoints[filtername] - simData[self.skybrightnessCol][in_filt])) * self.pixscale**2
-            sky_counts = sky_counts * simData[self.exptimeCol][in_filt]/simData[self.nexpCol][in_filt]
+            # Total sky counts in each exposure 
+            sky_counts = sky_counts * exptime
+            # The counts available to the source (at peak) in each exposure is the difference between saturation and sky
             remaining_counts_peak = (self.saturation_adu - sky_counts)
             # Now to figure out how many counts there would be total, if there are that many in the peak
             sigma = simData[self.seeingCol][in_filt]/2.354
             source_counts = remaining_counts_peak * 2.*np.pi*(sigma/self.pixscale)**2
-
-            count_rate = source_counts / simData[self.nexpCol][in_filt] / simData[self.exptimeCol][in_filt]
+            # source counts = counts per exposure (expTimeCol / nexp)
+            # Translate to counts per second, to apply zeropoint 
+            count_rate = source_counts / exptime
             simData['saturation_mag'][in_filt] = -2.5*np.log10(count_rate) + self.zeropoints[filtername]
             # Airmass correction
             simData['saturation_mag'][in_filt] -= self.km[filtername]*(simData[self.airmassCol][in_filt] - 1.)
